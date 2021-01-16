e22301b7014960e4cbd3ae26f27ee457386522ae
Yoooi <kamos21@protonmail.com>
2021-01-16 20:06:24 +0100

    Cleanup VideoSourceStatusToBrushConverter

--- MultiFunPlayer/Common/Converters/VideoSourceStatusToBrushConverter.cs
+++ MultiFunPlayer/Common/Converters/VideoSourceStatusToBrushConverter.cs
@@ -12,16 +12,14 @@ namespace MultiFunPlayer.Common.Converters
 {
     public class VideoSourceStatusToBrushConverter : IValueConverter
     {
-        private IReadOnlyDictionary<VideoSourceStatus, Color> _colors = new Dictionary<VideoSourceStatus, Color>()
-        {
-            { VideoSourceStatus.Connected, Color.FromRgb(0x00, 0x80, 0x00) },
-            { VideoSourceStatus.Disconnected, Color.FromRgb(0x00, 0x00, 0x00) },
-            { VideoSourceStatus.Connecting, Color.FromRgb(0xb3, 0x9c, 0x09) },
-            { VideoSourceStatus.Disconnecting, Color.FromRgb(0x0f, 0xa7, 0xa8) }
-        };
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => value is VideoSourceStatus status ? new SolidColorBrush(_colors[status]) : null;
+            => value switch
+            {
+                VideoSourceStatus.Connected => new SolidColorBrush(Color.FromRgb(0x00, 0x80, 0x00)),
+                VideoSourceStatus.Disconnected => new SolidColorBrush(Color.FromRgb(0x00, 0x00, 0x00)),
+                VideoSourceStatus.Connecting or VideoSourceStatus.Disconnecting => new SolidColorBrush(Color.FromRgb(0xb3, 0x9c, 0x09)),
+                _ => null
+            };
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => throw new NotSupportedException();
