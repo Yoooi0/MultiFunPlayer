19d1a8538468a7141a4051a19a163f1fc30f1723
Yoooi <kamos21@protonmail.com>
2021-02-14 22:00:33 +0100

    Implement named pipes output target

---
+++ MultiFunPlayer/OutputTarget/ViewModels/PipeOutputTargetViewModel.cs
@@ -0,0 +1,90 @@
+﻿using MaterialDesignThemes.Wpf;
+using MultiFunPlayer.Common;
+using MultiFunPlayer.Common.Controls;
+using MultiFunPlayer.Common.Messages;
+using Newtonsoft.Json.Linq;
+using Stylet;
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace MultiFunPlayer.OutputTarget.ViewModels
+{
+    public class PipeOutputTargetViewModel : AbstractOutputTarget
+    {
+        public override string Name => "Pipe";
+        public override OutputTargetStatus Status { get; protected set; }
+
+        public string PipeName { get; set; } = "mfp-pipe";
+
+        public PipeOutputTargetViewModel(IEventAggregator eventAggregator, IDeviceAxisValueProvider valueProvider)
+            : base(eventAggregator, valueProvider) { }
+
+        public bool IsConnected => Status == OutputTargetStatus.Connected;
+        public bool IsConnectBusy => Status == OutputTargetStatus.Connecting || Status == OutputTargetStatus.Disconnecting;
+        public bool CanToggleConnect => !IsConnectBusy;
+
+        protected override void Run(CancellationToken token)
+        {
+            var client = default(NamedPipeClientStream);
+
+            try
+            {
+                client = new NamedPipeClientStream(".", PipeName, PipeDirection.Out);
+                client.Connect();
+
+                Status = OutputTargetStatus.Connected;
+            }
+            catch (Exception e)
+            {
+                if (client?.IsConnected == true)
+                    client.Close();
+
+                _ = Execute.OnUIThreadAsync(() => _ = DialogHost.Show(new ErrorMessageDialog($"Error when opening pipe:\n\n{e}")));
+                return;
+            }
+
+            try
+            {
+                var sb = new StringBuilder(256);
+                var buffer = new byte[256];
+                while (!token.IsCancellationRequested)
+                {
+                    var interval = MathF.Max(1, 1000.0f / UpdateRate);
+                    UpdateValues();
+
+                    sb.Clear();
+                    foreach (var (axis, value) in Values)
+                    {
+                        sb.Append(axis)
+                          .AppendFormat("{0:000}", value * 999)
+                          .AppendFormat("I{0}", (int)interval)
+                          .Append(' ');
+                    }
+
+                    var encoded = Encoding.ASCII.GetBytes($"{sb}\n", buffer);
+                    if(client?.IsConnected == true)
+                        client?.Write(buffer, 0, encoded);
+
+                    Thread.Sleep((int)interval);
+                }
+            }
+            catch (Exception e)
+            {
+                _ = Execute.OnUIThreadAsync(() => _ = DialogHost.Show(new ErrorMessageDialog($"Unhandled error:\n\n{e}")));
+            }
+
+            if (client?.IsConnected == true)
+                client.Close();
+        }
+
+        protected override void HandleSettings(JObject settings, AppSettingsMessageType type)
+        {
+        }
+
+    }
+}
