8b0dabead316c048fde6b54ed2642dbff91292cc
Yoooi <kamos21@protonmail.com>
2021-04-27 01:39:40 +0200

    Move keyframe class to common

---
+++ MultiFunPlayer/Common/Keyframe.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace MultiFunPlayer.Common
+{
+    [DebuggerDisplay("[{Position}, {Value}]")]
+    public class Keyframe
+    {
+        public float Position { get; set; }
+        public float Value { get; set; }
+
+        public Keyframe(float position) : this(position, float.NaN) { }
+        public Keyframe(float position, float value)
+        {
+            Position = position;
+            Value = value;
+        }
+
+        public void Deconstruct(out float position, out float value)
+        {
+            position = Position;
+            value = Value;
+        }
+    }
+
+    public class KeyframePositionComparer : IComparer<Keyframe>
+    {
+        public int Compare(Keyframe x, Keyframe y)
+            => Comparer<float>.Default.Compare(x.Position, y.Position);
+    }
+}
--- MultiFunPlayer/ViewModels/ScriptViewModel.cs
+++ MultiFunPlayer/ViewModels/ScriptViewModel.cs
@@ -905,30 +905,4 @@ namespace MultiFunPlayer.ViewModels
         public IEnumerable<FileInfo> EnumerateFiles(string searchPattern)
             => Directory.EnumerateFiles(searchPattern, Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
     }
-
-    [DebuggerDisplay("[{Position}, {Value}]")]
-    public class Keyframe
-    {
-        public float Position { get; set; }
-        public float Value { get; set; }
-
-        public Keyframe(float position) : this(position, float.NaN) { }
-        public Keyframe(float position, float value)
-        {
-            Position = position;
-            Value = value;
-        }
-
-        public void Deconstruct(out float position, out float value)
-        {
-            position = Position;
-            value = Value;
-        }
-    }
-
-    public class KeyframePositionComparer : IComparer<Keyframe>
-    {
-        public int Compare(Keyframe x, Keyframe y)
-            => Comparer<float>.Default.Compare(x.Position, y.Position);
-    }
 }
