a0156c1f09993fd2e51c9b0a2e6a7e7e92447ebb
Yoooi <kamos21@protonmail.com>
2021-05-02 21:16:07 +0200

    Organize Extensions.cs

--- MultiFunPlayer/Common/Extensions.cs
+++ MultiFunPlayer/Common/Extensions.cs
@@ -3,7 +3,6 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
@@ -14,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace MultiFunPlayer.Common
 {
-    public static class Extensions
+    public static class JsonExtensions
     {
         public static bool TryToObject<T>(this JToken token, out T value)
         {
@@ -28,35 +27,59 @@ namespace MultiFunPlayer.Common
                 value = token.ToObject<T>();
                 return true;
             }
-            catch(FormatException)
+            catch (FormatException)
             {
                 return false;
             }
         }
 
-        public static async Task<byte[]> ReadAllBytesAsync(this NetworkStream stream, CancellationToken token)
+        public static bool EnsureContainsObjects(this JToken token, params string[] propertyNames)
         {
-            var result = 0;
-            var buffer = new ArraySegment<byte>(new byte[1024]);
-            using var memory = new MemoryStream();
-            do
-            {
-                result = await stream.ReadAsync(buffer, token);
-                await memory.WriteAsync(buffer.AsMemory(buffer.Offset, result), token);
-            }
-            while (result > 0 && stream.DataAvailable);
+            if (token is not JObject o)
+                return false;
 
-            memory.Seek(0, SeekOrigin.Begin);
-            return memory.ToArray();
+            foreach (var propertyName in propertyNames)
+            {
+                if (!o.ContainsKey(propertyName))
+                    o[propertyName] = new JObject();
+
+                if (o[propertyName] is JObject child)
+                    o = child;
+                else
+                    return false;
+            }
+
+            return true;
         }
 
-        public static ObservableConcurrentDictionaryView<TKey, TValue, TView> CreateView<TKey, TValue, TView>(
-            this ObservableConcurrentDictionary<TKey, TValue> dictionary, Expression<Func<TValue, TView>> selector) where TValue : class
-            => new(dictionary, selector);
+        public static bool TryGetObject(this JToken token, out JObject result, params string[] propertyNames)
+        {
+            result = null;
+            if (token is not JObject o)
+                return false;
 
-        public static ObservableConcurrentDictionaryView<TKey, TValue, TView> CreateView<TKey, TValue, TView>(
-            this ObservableConcurrentDictionary<TKey, TValue> dictionary, Func<TValue, TView> selector, string propertyName) where TValue : class
-            => new(dictionary, selector, propertyName);
+            foreach (var propertyName in propertyNames)
+            {
+                if (!o.ContainsKey(propertyName) || o[propertyName] is not JObject child)
+                    return false;
+
+                o = child;
+            }
+
+            result = o;
+            return true;
+        }
+
+        public static void Populate(this JToken token, object target)
+        {
+            using var reader = token.CreateReader();
+            JsonSerializer.CreateDefault().Populate(reader, target);
+        }
+
+    }
+
+    public static class TaskExtensions
+    {
 
         public static Task WithCancellation(this Task task, CancellationToken cancellationToken)
         {
@@ -89,6 +112,17 @@ namespace MultiFunPlayer.Common
                 return Task.FromCanceled<TResult>(cancellationToken);
             return DoWaitAsync(task, cancellationToken);
         }
+    }
+
+    public static class CollectionExtensions
+    {
+        public static ObservableConcurrentDictionaryView<TKey, TValue, TView> CreateView<TKey, TValue, TView>(
+            this ObservableConcurrentDictionary<TKey, TValue> dictionary, Expression<Func<TValue, TView>> selector) where TValue : class
+            => new(dictionary, selector);
+
+        public static ObservableConcurrentDictionaryView<TKey, TValue, TView> CreateView<TKey, TValue, TView>(
+            this ObservableConcurrentDictionary<TKey, TValue> dictionary, Func<TValue, TView> selector, string propertyName) where TValue : class
+            => new(dictionary, selector, propertyName);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool ValidateIndex<T>(this ICollection<T> collection, int index)
@@ -101,13 +135,13 @@ namespace MultiFunPlayer.Common
                 return false;
 
             var o = list[index];
-            if(o == null)
+            if (o == null)
                 return !typeof(T).IsValueType || Nullable.GetUnderlyingType(typeof(T)) != null;
 
             if (o is not T)
                 return false;
 
-            value = (T) o;
+            value = (T)o;
             return true;
         }
 
@@ -120,76 +154,24 @@ namespace MultiFunPlayer.Common
             value = list[index];
             return true;
         }
+    }
 
-        public static bool EnsureContainsObjects(this JToken token, params string[] propertyNames)
+    public static class StreamExtensions
+    {
+        public static async Task<byte[]> ReadAllBytesAsync(this NetworkStream stream, CancellationToken token)
         {
-            if (token is not JObject o)
-                return false;
-
-            foreach (var propertyName in propertyNames)
+            var result = 0;
+            var buffer = new ArraySegment<byte>(new byte[1024]);
+            using var memory = new MemoryStream();
+            do
             {
-                if (!o.ContainsKey(propertyName))
-                    o[propertyName] = new JObject();
-
-                if (o[propertyName] is JObject child)
-                    o = child;
-                else
-                    return false;
+                result = await stream.ReadAsync(buffer, token);
+                await memory.WriteAsync(buffer.AsMemory(buffer.Offset, result), token);
             }
+            while (result > 0 && stream.DataAvailable);
 
-            return true;
-        }
-
-        public static bool TryGetObject(this JToken token, out JObject result, params string[] propertyNames)
-        {
-            result = null;
-            if (token is not JObject o)
-                return false;
-
-            foreach(var propertyName in propertyNames)
-            {
-                if (!o.ContainsKey(propertyName) || o[propertyName] is not JObject child)
-                    return false;
-
-                o = child;
-            }
-
-            result = o;
-            return true;
-        }
-
-        public static void Populate(this JToken token, object target)
-        {
-            using var reader = token.CreateReader();
-            JsonSerializer.CreateDefault().Populate(reader, target);
-        }
-
-        public static void PreciseSleep(this Stopwatch stopwatch, float interval, CancellationToken token)
-        {
-            var nextTrigger = stopwatch.ElapsedMilliseconds + interval;
-            if (stopwatch.Elapsed.TotalHours >= 1d)
-            {
-                stopwatch.Restart();
-                nextTrigger = 0f;
-            }
-
-            var tickLength = 1000f / Stopwatch.Frequency;
-            while (!token.IsCancellationRequested)
-            {
-                var elapsed = stopwatch.ElapsedTicks * tickLength;
-                var diff = nextTrigger - elapsed;
-                if (diff <= 0f)
-                    break;
-
-                if (diff < 1f)
-                    Thread.SpinWait(10);
-                else if (diff < 5f)
-                    Thread.SpinWait(100);
-                else if (diff < 15f)
-                    Thread.Sleep(1);
-                else
-                    Thread.Sleep(10);
-            }
+            memory.Seek(0, SeekOrigin.Begin);
+            return memory.ToArray();
         }
     }
 
