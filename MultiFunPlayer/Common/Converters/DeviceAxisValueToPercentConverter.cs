caa38bcf2a66d897ec560e2aa2cfd7605a7a584f
Yoooi <kamos21@protonmail.com>
2021-05-03 17:49:30 +0200

    Fix visual jitter of axis value progress bar

--- MultiFunPlayer/Common/Converters/DeviceAxisValueToPercentConverter.cs
+++ MultiFunPlayer/Common/Converters/DeviceAxisValueToPercentConverter.cs
@@ -7,7 +7,7 @@ namespace MultiFunPlayer.Common.Converters
     public class DeviceAxisValueToPercentConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => value is float x && float.IsFinite(x) ? MathUtils.Clamp(x * 100, 0, 100) : 0;
+            => value is float x && float.IsFinite(x) ? MathUtils.Clamp(MathF.Round(x, 2) * 100, 0, 100) : 0;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => value is float x && float.IsFinite(x) ? MathUtils.Clamp01(x / 100) : float.NaN;
