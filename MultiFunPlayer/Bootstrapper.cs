23aa17ee288210d98df3b38566a5a43cf71e92e3
Yoooi <kamos21@protonmail.com>
2021-05-25 01:02:04 +0200

    Move settings handling to static class

--- MultiFunPlayer/Bootstrapper.cs
+++ MultiFunPlayer/Bootstrapper.cs
@@ -1,8 +1,17 @@
-﻿using MultiFunPlayer.OutputTarget;
+using MultiFunPlayer.Common;
+using MultiFunPlayer.Common.Converters;
+using MultiFunPlayer.OutputTarget;
 using MultiFunPlayer.VideoSource;
 using MultiFunPlayer.ViewModels;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 using Stylet;
 using StyletIoC;
+using System.Diagnostics;
 
 namespace MultiFunPlayer
 {
@@ -13,6 +22,80 @@ namespace MultiFunPlayer
             builder.Bind<ScriptViewModel>().And<IDeviceAxisValueProvider>().To<ScriptViewModel>().InSingletonScope();
             builder.Bind<IVideoSource>().ToAllImplementations();
             builder.Bind<IOutputTarget>().ToAllImplementations();
+        }
+
+        protected override void OnStart()
+        {
+            SetupLoging();
+            SetupJson();
+        }
+
+        private void SetupJson()
+        {
+            var logger = LogManager.GetLogger(nameof(JsonConvert));
+            JsonConvert.DefaultSettings = () =>
+            {
+                var settings = new JsonSerializerSettings
+                {
+                    Formatting = Formatting.Indented
+                };
+
+                settings.Converters.Add(new FileSystemInfoConverter());
+                settings.Converters.Add(new StringEnumConverter());
+                settings.Error += (s, e) =>
+                {
+                    if (e.ErrorContext.Error is JsonSerializationException or JsonReaderException)
+                    {
+                        logger.Warn(e.ErrorContext.Error);
+                        e.ErrorContext.Handled = true;
+                    }
+                };
+
+                return settings;
+            };
+        }
+
+        private static void SetupLoging()
+        {
+            var settings = Settings.Read();
+            if (!settings.ContainsKey("LogLevel"))
+            {
+                settings["LogLevel"] = JToken.FromObject(LogLevel.Info);
+                Settings.Write(settings);
+            }
+
+            if (settings.TryGetValue<string>("LogLevel", out var logLevel))
+            {
+                var config = new LoggingConfiguration();
+                const string layout = "${longdate}|${level:uppercase=true}|${logger}|${message}${onexception:|${exception:format=ToString}}";
+
+                config.AddRule(LogLevel.FromString(logLevel), LogLevel.Fatal, new FileTarget("file")
+                {
+                    FileName = @"${basedir}\Logs\latest.log",
+                    ArchiveFileName = @"${basedir}\Logs\log.{#}.log",
+                    ArchiveNumbering = ArchiveNumberingMode.DateAndSequence,
+                    ArchiveAboveSize = 1048576,
+                    ArchiveDateFormat = "yyyyMMdd",
+                    ArchiveOldFileOnStartup = true,
+                    MaxArchiveFiles = 10,
+                    ConcurrentWrites = false,
+                    KeepFileOpen = true,
+                    OpenFileCacheTimeout = 30,
+                    AutoFlush = false,
+                    OpenFileFlushTimeout = 5,
+                    Layout = layout
+                });
+
+                if (Debugger.IsAttached)
+                {
+                    config.AddRule(LogLevel.Debug, LogLevel.Fatal, new OutputDebugStringTarget("debug")
+                    {
+                        Layout = layout
+                    });
+                }
+
+                LogManager.Configuration = config;
+            }
         }
     }
 }
--- MultiFunPlayer/ViewModels/RootViewModel.cs
+++ MultiFunPlayer/ViewModels/RootViewModel.cs
@@ -32,67 +32,6 @@ namespace MultiFunPlayer.ViewModels
         public RootViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
-
-            JsonConvert.DefaultSettings = () =>
-            {
-                var settings = new JsonSerializerSettings
-                {
-                    Formatting = Formatting.Indented
-                };
-
-                settings.Converters.Add(new FileSystemInfoConverter());
-                settings.Converters.Add(new StringEnumConverter());
-                settings.Error += (s, e) =>
-                {
-                    if (e.ErrorContext.Error is JsonSerializationException or JsonReaderException)
-                    {
-                        Logger.Warn(e.ErrorContext.Error);
-                        e.ErrorContext.Handled = true;
-                    }
-                };
-
-                return settings;
-            };
-
-            var settings = ReadSettings();
-            if (!settings.ContainsKey("LogLevel"))
-            {
-                settings["LogLevel"] = JToken.FromObject(LogLevel.Info);
-                WriteSettings(settings);
-            }
-
-            if (settings.TryGetValue<string>("LogLevel", out var logLevel))
-            {
-                var config = new LoggingConfiguration();
-                const string layout = "${longdate}|${level:uppercase=true}|${logger}|${message}${onexception:|${exception:format=ToString}}";
-
-                config.AddRule(LogLevel.FromString(logLevel), LogLevel.Fatal, new FileTarget("file")
-                {
-                    FileName = @"${basedir}\Logs\latest.log",
-                    ArchiveFileName = @"${basedir}\Logs\log.{#}.log",
-                    ArchiveNumbering = ArchiveNumberingMode.DateAndSequence,
-                    ArchiveAboveSize = 1048576,
-                    ArchiveDateFormat = "yyyyMMdd",
-                    ArchiveOldFileOnStartup = true,
-                    MaxArchiveFiles = 10,
-                    ConcurrentWrites = false,
-                    KeepFileOpen = true,
-                    OpenFileCacheTimeout = 30,
-                    AutoFlush = false,
-                    OpenFileFlushTimeout = 5,
-                    Layout = layout
-                });
-
-                if (Debugger.IsAttached)
-                {
-                    config.AddRule(LogLevel.Debug, LogLevel.Fatal, new OutputDebugStringTarget("debug")
-                    {
-                        Layout = layout
-                    });
-                }
-
-                LogManager.Configuration = config;
-            }
         }
 
         protected override void OnActivate()
@@ -104,7 +43,7 @@ namespace MultiFunPlayer.ViewModels
             ActivateAndSetParent(Items);
             base.OnActivate();
 
-            var settings = ReadSettings();
+            var settings = Settings.Read();
             _eventAggregator.Publish(new AppSettingsMessage(settings, AppSettingsMessageType.Loading));
         }
 
@@ -115,7 +54,7 @@ namespace MultiFunPlayer.ViewModels
         {
             Execute.PostToUIThread(async () =>
             {
-                var settings = ReadSettings();
+                var settings = Settings.Read();
                 if (!settings.TryGetValue("DisablePopup", out var disablePopupToken) || !disablePopupToken.Value<bool>())
                 {
                     var result = await DialogHost.Show(new InformationMessageDialog(showCheckbox: true)).ConfigureAwait(true);
@@ -123,49 +62,16 @@ namespace MultiFunPlayer.ViewModels
                         return;
 
                     settings["DisablePopup"] = true;
-                    WriteSettings(settings);
+                    Settings.Write(settings);
                 }
             });
         }
 
         public void OnClosing(object sender, EventArgs e)
         {
-            var settings = ReadSettings();
+            var settings = Settings.Read();
             _eventAggregator.Publish(new AppSettingsMessage(settings, AppSettingsMessageType.Saving));
-            WriteSettings(settings);
-        }
-
-        private JObject ReadSettings()
-        {
-            var path = Path.Join(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "MultiFunPlayer.config.json");
-            if (!File.Exists(path))
-                return new JObject();
-
-            Logger.Info("Reading settings from \"{0}\"", path);
-            try
-            {
-                return JObject.Parse(File.ReadAllText(path));
-            }
-            catch (Exception e)
-            {
-                Logger.Error(e, "Failed to read settings");
-                return new JObject();
-            }
-        }
-
-        private void WriteSettings(JObject settings)
-        {
-            try
-            {
-                var path = Path.Join(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "MultiFunPlayer.config.json");
-
-                Logger.Info("Saving settings to \"{0}\"", path);
-                File.WriteAllText(path, settings.ToString());
-            }
-            catch (Exception e)
-            {
-                Logger.Error(e, "Failed to save settings");
-            }
+            Settings.Write(settings);
         }
 
         public void OnMouseDown(object sender, MouseButtonEventArgs e)
