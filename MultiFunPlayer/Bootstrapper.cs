e79a9e5a023142d350228372f73a0a79431e82eb
Yoooi <kamos21@protonmail.com>
2021-06-01 23:51:07 +0200

    Log unhandled exceptions

--- MultiFunPlayer/Bootstrapper.cs
+++ MultiFunPlayer/Bootstrapper.cs
@@ -1,8 +1,10 @@
 ﻿using MultiFunPlayer.OutputTarget;
 using MultiFunPlayer.VideoSource;
 using MultiFunPlayer.ViewModels;
+using NLog;
 using Stylet;
 using StyletIoC;
+using System;
 
 namespace MultiFunPlayer
 {
@@ -13,6 +15,12 @@ namespace MultiFunPlayer
             builder.Bind<ScriptViewModel>().And<IDeviceAxisValueProvider>().To<ScriptViewModel>().InSingletonScope();
             builder.Bind<IVideoSource>().ToAllImplementations();
             builder.Bind<IOutputTarget>().ToAllImplementations();
+        }
+
+        protected override void OnStart()
+        {
+            var logger = LogManager.GetLogger(nameof(AppDomain));
+            AppDomain.CurrentDomain.UnhandledException += (s, e) => logger.Fatal(e.ExceptionObject as Exception);
         }
     }
 }
