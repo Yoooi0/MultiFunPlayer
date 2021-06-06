864186baca7fd36f4c7558d523dd8afd86ff6b89
Yoooi <kamos21@protonmail.com>
2021-06-06 16:56:44 +0200

    Fix TCode whitespace

--- MultiFunPlayer/Common/DataFormat.cs
+++ MultiFunPlayer/Common/DataFormat.cs
@@ -10,7 +10,7 @@ namespace MultiFunPlayer.Common
         public static string ToString(DeviceAxis axis, float value, int interval) => $"{ToString(axis, value)}I{interval}";
 
         public static string ToString(IEnumerable<KeyValuePair<DeviceAxis, float>> values, int interval)
-            => $"{values.Aggregate(string.Empty, (s, x) => $"{s} {ToString(x.Key, x.Value, interval)}")}\n";
+            => $"{values.Aggregate(string.Empty, (s, x) => $"{s} {ToString(x.Key, x.Value, interval)}")}\n".TrimStart();
 
         public static bool IsDirty(float value, float lastValue)
             => float.IsFinite(value) && (!float.IsFinite(lastValue) || MathF.Abs(lastValue - value) * 999 >= 1);
