3a26cfca340859d3ddbe1612f6bf37aebe4c4477
Yoooi <kamos21@protonmail.com>
2021-06-13 20:47:54 +0200

    Add log name blacklist to settings

--- MultiFunPlayer/Bootstrapper.cs
+++ MultiFunPlayer/Bootstrapper.cs
@@ -1,7 +1,8 @@
-﻿using MultiFunPlayer.Common;
+using MultiFunPlayer.Common;
 using MultiFunPlayer.Common.Converters;
 using MultiFunPlayer.Common.Input;
 using MultiFunPlayer.Common.Input.RawInput;
+using MultiFunPlayer.Common.Input.XInput;
 using MultiFunPlayer.OutputTarget;
 using MultiFunPlayer.VideoSource;
 using MultiFunPlayer.ViewModels;
@@ -14,6 +15,7 @@ using NLog.Targets;
 using Stylet;
 using StyletIoC;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
@@ -35,8 +37,8 @@ namespace MultiFunPlayer
 
         protected override void OnStart()
         {
-            SetupLoging();
             SetupJson();
+            SetupLoging();
 
             var logger = LogManager.GetLogger(nameof(AppDomain));
             AppDomain.CurrentDomain.UnhandledException += (s, e) =>
@@ -67,6 +69,7 @@ namespace MultiFunPlayer
                     Formatting = Formatting.Indented
                 };
 
+                settings.Converters.Add(new LogLevelConverter());
                 settings.Converters.Add(new FileSystemInfoConverter());
                 settings.Converters.Add(new StringEnumConverter());
                 settings.Error += (s, e) =>
@@ -85,18 +88,40 @@ namespace MultiFunPlayer
         private static void SetupLoging()
         {
             var settings = Settings.Read();
+            var settingsDirty = false;
             if (!settings.ContainsKey("LogLevel"))
             {
+                settingsDirty = true;
                 settings["LogLevel"] = JToken.FromObject(LogLevel.Info);
-                Settings.Write(settings);
             }
 
-            if (settings.TryGetValue<string>("LogLevel", out var logLevel))
+            if (!settings.ContainsKey("LogBlacklist"))
             {
-                var config = new LoggingConfiguration();
-                const string layout = "${longdate}|${level:uppercase=true}|${logger}|${message}${onexception:|${exception:format=ToString}}";
+                settingsDirty = true;
+                settings["LogBlacklist"] = JObject.FromObject(new Dictionary<string, LogLevel>() {
+                    [$"{typeof(RawInputProcessor).Namespace}.*"] = LogLevel.Trace,
+                    [$"{typeof(XInputProcessor).Namespace}.*"] = LogLevel.Trace,
+                    [$"{typeof(ShortcutViewModel).FullName}"] = LogLevel.Trace
+                });
+            }
 
-                config.AddRule(LogLevel.FromString(logLevel), LogLevel.Fatal, new FileTarget("file")
+            if (settingsDirty)
+                Settings.Write(settings);
+
+            var config = new LoggingConfiguration();
+            const string layout = "${longdate}|${level:uppercase=true}|${logger}|${message}${onexception:|${exception:format=ToString}}";
+
+            if(settings.TryGetValue<Dictionary<string, LogLevel>>("LogBlacklist", out var blacklist))
+            {
+                var blackhole = new NullTarget();
+                foreach (var (filter, maxLevel) in blacklist)
+                    config.AddRule(LogLevel.Trace, maxLevel, blackhole, filter, true);
+            }
+
+
+            if (settings.TryGetValue<LogLevel>("LogLevel", out var minLevel))
+            {
+                config.AddRule(minLevel, LogLevel.Fatal, new FileTarget("file")
                 {
                     FileName = @"${basedir}\Logs\latest.log",
                     ArchiveFileName = @"${basedir}\Logs\log.{#}.log",
@@ -112,17 +137,17 @@ namespace MultiFunPlayer
                     OpenFileFlushTimeout = 5,
                     Layout = layout
                 });
-
-                if (Debugger.IsAttached)
-                {
-                    config.AddRule(LogLevel.Debug, LogLevel.Fatal, new OutputDebugStringTarget("debug")
-                    {
-                        Layout = layout
-                    });
-                }
-
-                LogManager.Configuration = config;
             }
+
+            if (Debugger.IsAttached)
+            {
+                config.AddRule(LogLevel.Debug, LogLevel.Fatal, new OutputDebugStringTarget("debug")
+                {
+                    Layout = layout
+                });
+            }
+
+            LogManager.Configuration = config;
         }
     }
 }
