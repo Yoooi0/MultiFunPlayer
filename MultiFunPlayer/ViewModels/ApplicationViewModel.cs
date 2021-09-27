43b06fa56b905ee0a6db2de71c5d80a1d16ba66d
Yoooi <kamos21@protonmail.com>
2021-09-27 18:49:23 +0200

    Add ability to set the window to be always on top (#23)

--- MultiFunPlayer/ViewModels/ApplicationViewModel.cs
+++ MultiFunPlayer/ViewModels/ApplicationViewModel.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json.Linq;
 using Stylet;
 using System.IO;
 using System.Linq;
+using System.Windows;
 
 namespace MultiFunPlayer.ViewModels
 {
@@ -11,6 +12,7 @@ namespace MultiFunPlayer.ViewModels
     {
         public BindableCollection<string> DeviceTypes { get; }
         public string SelectedDevice { get; set; }
+        public bool AlwaysOnTop { get; set; }
 
         public ApplicationViewModel(IEventAggregator eventAggregator)
         {
@@ -20,16 +22,25 @@ namespace MultiFunPlayer.ViewModels
             DeviceTypes = new BindableCollection<string>(devices);
         }
 
+        public void OnAlwaysOnTopChanged()
+        {
+            Application.Current.MainWindow.Topmost = AlwaysOnTop;
+        }
+
         public void Handle(AppSettingsMessage message)
         {
             if (message.Type == AppSettingsMessageType.Saving)
             {
                 message.Settings[nameof(SelectedDevice)] = SelectedDevice;
+                message.Settings[nameof(AlwaysOnTop)] = AlwaysOnTop;
             }
             else if (message.Type == AppSettingsMessageType.Loading)
             {
                 if(message.Settings.TryGetValue<string>(nameof(SelectedDevice), out var selectedDevice))
                     SelectedDevice = selectedDevice;
+
+                if (message.Settings.TryGetValue<bool>(nameof(AlwaysOnTop), out var alwaysOnTop))
+                    AlwaysOnTop = alwaysOnTop;
             }
         }
     }
