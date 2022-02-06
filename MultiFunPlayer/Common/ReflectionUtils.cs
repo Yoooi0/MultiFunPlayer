4d98708b2788b39f37b55673a952e7ff00d9864c
Yoooi <kamos21@protonmail.com>
2022-02-06 22:49:06 +0100

    Move helper methods from TypedValueConverter to ReflectionUtils

--- MultiFunPlayer/Common/ReflectionUtils.cs
+++ MultiFunPlayer/Common/ReflectionUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Text;
 
 namespace MultiFunPlayer.Common;
 
@@ -17,4 +18,105 @@ public static class ReflectionUtils
         => baseType.IsInterface
             ? baseType.IsAssignableFrom(otherType)
             : otherType.IsSubclassOf(baseType) || baseType == otherType;
+
+    public static string RemoveAssemblyDetails(string fullyQualifiedTypeName)
+    {
+        var builder = new StringBuilder();
+
+        var writingAssemblyName = false;
+        var skippingAssemblyDetails = false;
+        var followBrackets = false;
+        for (var i = 0; i < fullyQualifiedTypeName.Length; i++)
+        {
+            var current = fullyQualifiedTypeName[i];
+            switch (current)
+            {
+                case '[':
+                    writingAssemblyName = false;
+                    skippingAssemblyDetails = false;
+                    followBrackets = true;
+                    builder.Append(current);
+                    break;
+                case ']':
+                    writingAssemblyName = false;
+                    skippingAssemblyDetails = false;
+                    followBrackets = false;
+                    builder.Append(current);
+                    break;
+                case ',':
+                    if (followBrackets)
+                    {
+                        builder.Append(current);
+                    }
+                    else if (!writingAssemblyName)
+                    {
+                        writingAssemblyName = true;
+                        builder.Append(current);
+                    }
+                    else
+                    {
+                        skippingAssemblyDetails = true;
+                    }
+
+                    break;
+                default:
+                    followBrackets = false;
+                    if (!skippingAssemblyDetails)
+                        builder.Append(current);
+                    break;
+            }
+        }
+
+        return builder.ToString();
+    }
+
+    public static (string AssemblyName, string TypeName) SplitFullyQualifiedTypeName(string fullyQualifiedTypeName)
+    {
+        static int? GetAssemblyDelimiterIndex(string fullyQualifiedTypeName)
+        {
+            var scope = 0;
+            for (var i = 0; i < fullyQualifiedTypeName.Length; i++)
+            {
+                var current = fullyQualifiedTypeName[i];
+                switch (current)
+                {
+                    case '[':
+                        scope++;
+                        break;
+                    case ']':
+                        scope--;
+                        break;
+                    case ',':
+                        if (scope == 0)
+                            return i;
+                        break;
+                }
+            }
+
+            return null;
+        }
+
+        static string Trim(string s, int start, int length)
+        {
+            var end = start + length - 1;
+            for (; start < end; start++)
+                if (!char.IsWhiteSpace(s[start]))
+                    break;
+
+            for (; end >= start; end--)
+                if (!char.IsWhiteSpace(s[end]))
+                    break;
+
+            return s.Substring(start, end - start + 1);
+        }
+
+        var assemblyDelimiterIndex = GetAssemblyDelimiterIndex(fullyQualifiedTypeName);
+        if (assemblyDelimiterIndex != null)
+        {
+            return (Trim(fullyQualifiedTypeName, (int)assemblyDelimiterIndex + 1, fullyQualifiedTypeName.Length - (int)assemblyDelimiterIndex - 1),
+                    Trim(fullyQualifiedTypeName, 0, (int)assemblyDelimiterIndex));
+        }
+
+        return (null, fullyQualifiedTypeName);
+    }
 }
--- MultiFunPlayer/Settings/Converters/TypedValueConverter.cs
+++ MultiFunPlayer/Settings/Converters/TypedValueConverter.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using MultiFunPlayer.Common;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Text;
 
@@ -6,112 +7,11 @@ namespace MultiFunPlayer.Settings.Converters;
 
 public class TypedValueConverter : JsonConverter<TypedValue>
 {
-    private string RemoveAssemblyDetails(string fullyQualifiedTypeName)
-    {
-        var builder = new StringBuilder();
-
-        var writingAssemblyName = false;
-        var skippingAssemblyDetails = false;
-        var followBrackets = false;
-        for (var i = 0; i < fullyQualifiedTypeName.Length; i++)
-        {
-            var current = fullyQualifiedTypeName[i];
-            switch (current)
-            {
-                case '[':
-                    writingAssemblyName = false;
-                    skippingAssemblyDetails = false;
-                    followBrackets = true;
-                    builder.Append(current);
-                    break;
-                case ']':
-                    writingAssemblyName = false;
-                    skippingAssemblyDetails = false;
-                    followBrackets = false;
-                    builder.Append(current);
-                    break;
-                case ',':
-                    if (followBrackets)
-                    {
-                        builder.Append(current);
-                    }
-                    else if (!writingAssemblyName)
-                    {
-                        writingAssemblyName = true;
-                        builder.Append(current);
-                    }
-                    else
-                    {
-                        skippingAssemblyDetails = true;
-                    }
-
-                    break;
-                default:
-                    followBrackets = false;
-                    if (!skippingAssemblyDetails)
-                        builder.Append(current);
-                    break;
-            }
-        }
-
-        return builder.ToString();
-    }
-
-    public static (string AssemblyName, string TypeName) SplitFullyQualifiedTypeName(string fullyQualifiedTypeName)
-    {
-        static int? GetAssemblyDelimiterIndex(string fullyQualifiedTypeName)
-        {
-            var scope = 0;
-            for (var i = 0; i < fullyQualifiedTypeName.Length; i++)
-            {
-                var current = fullyQualifiedTypeName[i];
-                switch (current)
-                {
-                    case '[':
-                        scope++;
-                        break;
-                    case ']':
-                        scope--;
-                        break;
-                    case ',':
-                        if (scope == 0)
-                            return i;
-                        break;
-                }
-            }
-
-            return null;
-        }
-
-        static string Trim(string s, int start, int length)
-        {
-            var end = start + length - 1;
-            for (; start < end; start++)
-                if (!char.IsWhiteSpace(s[start]))
-                    break;
-
-            for (; end >= start; end--)
-                if (!char.IsWhiteSpace(s[end]))
-                    break;
-
-            return s.Substring(start, end - start + 1);
-        }
-
-        var assemblyDelimiterIndex = GetAssemblyDelimiterIndex(fullyQualifiedTypeName);
-        if (assemblyDelimiterIndex != null)
-        {
-            return (Trim(fullyQualifiedTypeName, (int)assemblyDelimiterIndex + 1, fullyQualifiedTypeName.Length - (int)assemblyDelimiterIndex - 1),
-                    Trim(fullyQualifiedTypeName, 0, (int)assemblyDelimiterIndex));
-        }
-
-        return (null, fullyQualifiedTypeName);
-    }
-
     public override TypedValue ReadJson(JsonReader reader, Type objectType, TypedValue existingValue, bool hasExistingValue, JsonSerializer serializer)
     {
         var o = JToken.ReadFrom(reader) as JObject;
 
-        var (_, valueTypeName) = SplitFullyQualifiedTypeName(o["$type"].ToString());
+        var (_, valueTypeName) = ReflectionUtils.SplitFullyQualifiedTypeName(o["$type"].ToString());
         var valueType = Type.GetType(valueTypeName);
 
         if (o.ContainsKey("Value"))
@@ -126,7 +26,7 @@ public class TypedValueConverter : JsonConverter<TypedValue>
         var valueToken = value.Value == null ? JValue.CreateNull() : JToken.FromObject(value.Value, serializer);
         var o = new JObject
         {
-            { "$type", RemoveAssemblyDetails(value.Type.AssemblyQualifiedName) }
+            { "$type", ReflectionUtils.RemoveAssemblyDetails(value.Type.AssemblyQualifiedName) }
         };
 
         if (valueToken.Type == JTokenType.Object)
