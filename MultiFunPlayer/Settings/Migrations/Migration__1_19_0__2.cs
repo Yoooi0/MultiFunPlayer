faef4b0d2842a4e93de37a4b7c5fd78607d911eb
Yoooi <kamos21@protonmail.com>
2022-02-17 21:11:32 +0100

    Fix LogBlacklist by migrating to new namespaces

---
+++ MultiFunPlayer/Settings/Migrations/Migration__1_19_0__2.cs
@@ -0,0 +1,50 @@
+﻿using MultiFunPlayer.Common;
+using MultiFunPlayer.Input.RawInput;
+using MultiFunPlayer.Input.XInput;
+using MultiFunPlayer.UI.Controls.ViewModels;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiFunPlayer.Settings.Migrations;
+
+internal class Migration__1_19_0__2 : AbstractConfigMigration
+{
+    public override int TargetVersion => 2;
+
+    public override void Migrate(JObject settings)
+    {
+        if (settings.TryGetObject(out var logBlacklistSettings, "LogBlacklist"))
+        {
+            MigrateLogBlacklist(logBlacklistSettings);
+        }
+
+        base.Migrate(settings);
+    }
+
+    private void MigrateLogBlacklist(JObject settings)
+    {
+        Logger.Info("Migrating LogBlacklist");
+        var filterMap = new Dictionary<string, string>()
+        {
+            ["MultiFunPlayer.Common.Input.RawInput.*"] = $"{typeof(RawInputProcessor).Namespace}.*",
+            ["MultiFunPlayer.Common.Input.XInput.*"] = $"{typeof(XInputProcessor).Namespace}.*",
+            ["MultiFunPlayer.ViewModels.ShortcutViewModel"] = $"{typeof(ShortcutViewModel).FullName}"
+        };
+
+        foreach(var (from, to) in filterMap)
+        {
+            if (!settings.ContainsKey(from))
+                continue;
+
+            var value = settings[from];
+            settings.Remove(from);
+            settings.Add(to, value);
+
+            Logger.Info("Migrated from \"{0}\" to \"{1}\"", from, to);
+        }
+    }
+}
