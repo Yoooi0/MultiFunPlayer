f0c74d352ff314b3391e32cc80795e9d958a23c0
Yoooi <kamos21@protonmail.com>
2022-02-20 16:00:52 +0100

    Better explanation of motion provider blend

---
+++ MultiFunPlayer/UI/Converters/InvertValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MultiFunPlayer.UI.Converters;
+
+public class InvertValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (parameter is not string s)
+            throw new ArgumentException("Converter parameter must be provided");
+        return Invert(value, s);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (parameter is not string s)
+            throw new ArgumentException("Converter parameter must be provided");
+        return Invert(value, s);
+    }
+
+    private object Invert(object value, string parameter)
+    {
+        if (value is int i) return int.Parse(parameter) - i;
+        if (value is double d) return double.Parse(parameter) - d;
+        if (value is float f) return float.Parse(parameter) - f;
+        throw new NotSupportedException();
+    }
+}
