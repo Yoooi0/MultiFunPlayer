ba2da3d6ea81f9797ac1473329d8337ea9b6efcd
Yoooi <kamos21@protonmail.com>
2022-02-20 15:06:18 +0100

    Add ability to change log level from UI

--- MultiFunPlayer/UI/Controls/ViewModels/ApplicationViewModel.cs
+++ MultiFunPlayer/UI/Controls/ViewModels/ApplicationViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using MultiFunPlayer.Common;
 using MultiFunPlayer.Common.Messages;
 using MultiFunPlayer.Settings;
+using Newtonsoft.Json.Linq;
+using NLog;
 using Stylet;
 using System.Windows;
 
@@ -8,6 +10,10 @@ namespace MultiFunPlayer.UI.Controls.ViewModels;
 
 public class ApplicationViewModel : Screen, IHandle<AppSettingsMessage>
 {
+    protected Logger Logger = LogManager.GetCurrentClassLogger();
+
+    public ObservableConcurrentCollection<LogLevel> LogLevels { get; }
+    public LogLevel SelectedLogLevel { get; set; }
     public ObservableConcurrentCollection<string> DeviceTypes { get; }
     public string SelectedDevice { get; set; }
     public bool AlwaysOnTop { get; set; }
@@ -15,6 +21,8 @@ public class ApplicationViewModel : Screen, IHandle<AppSettingsMessage>
     public ApplicationViewModel(IEventAggregator eventAggregator)
     {
         eventAggregator.Subscribe(this);
+
+        LogLevels = new ObservableConcurrentCollection<LogLevel>(LogLevel.AllLevels);
 
         var devices = SettingsHelper.Read(SettingsType.Devices).Properties().Select(p => p.Name);
         DeviceTypes = new ObservableConcurrentCollection<string>(devices);
@@ -25,12 +33,26 @@ public class ApplicationViewModel : Screen, IHandle<AppSettingsMessage>
         Application.Current.MainWindow.Topmost = AlwaysOnTop;
     }
 
+    public void OnSelectedLogLevelChanged()
+    {
+        if (SelectedLogLevel == null)
+            return;
+
+        Logger.Info("Changing log level to \"{0}\"", SelectedLogLevel.Name);
+
+        var rule = LogManager.Configuration.LoggingRules.FirstOrDefault(r => r.Targets.Any(t => string.Equals(t.Name, "file", StringComparison.OrdinalIgnoreCase)));
+        rule?.SetLoggingLevels(SelectedLogLevel, LogLevel.Fatal);
+    }
+
     public void Handle(AppSettingsMessage message)
     {
         if (message.Type == AppSettingsMessageType.Saving)
         {
-            message.Settings[nameof(SelectedDevice)] = SelectedDevice;
-            message.Settings[nameof(AlwaysOnTop)] = AlwaysOnTop;
+            message.Settings[nameof(SelectedDevice)] = JToken.FromObject(SelectedDevice);
+            message.Settings[nameof(AlwaysOnTop)] = JToken.FromObject(AlwaysOnTop);
+
+            if(SelectedLogLevel != null)
+                message.Settings["LogLevel"] = JToken.FromObject(SelectedLogLevel);
         }
         else if (message.Type == AppSettingsMessageType.Loading)
         {
@@ -39,6 +61,9 @@ public class ApplicationViewModel : Screen, IHandle<AppSettingsMessage>
 
             if (message.Settings.TryGetValue<bool>(nameof(AlwaysOnTop), out var alwaysOnTop))
                 AlwaysOnTop = alwaysOnTop;
+
+            if (message.Settings.TryGetValue<LogLevel>("LogLevel", out var logLevel))
+                SelectedLogLevel = logLevel;
         }
     }
 }
