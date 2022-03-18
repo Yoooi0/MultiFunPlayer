ca0e4e72f15e1dc80ffd6ff0967bbe0340cc32b2
Yoooi <kamos21@protonmail.com>
2022-03-18 17:19:35 +0100

    Cleanup

--- MultiFunPlayer/UI/Converters/ButtplugClientDeviceConnectedConverter.cs
+++ MultiFunPlayer/UI/Converters/ButtplugClientDeviceConnectedConverter.cs
@@ -3,21 +3,20 @@ using MultiFunPlayer.OutputTarget.ViewModels;
 using System.Globalization;
 using System.Windows.Data;
 
-namespace MultiFunPlayer.UI.Converters
+namespace MultiFunPlayer.UI.Converters;
+
+public class ButtplugClientDeviceConnectedConverter : IMultiValueConverter
 {
-    public class ButtplugClientDeviceConnectedConverter : IMultiValueConverter
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (values.Length != 2 
-             || values[0] is not ButtplugClientDeviceSettings settings 
-             || values[1] is not IList<ButtplugClientDevice> availableDevices)
-                return false;
+        if (values.Length != 2 
+         || values[0] is not ButtplugClientDeviceSettings settings 
+         || values[1] is not IList<ButtplugClientDevice> availableDevices)
+            return false;
 
-            return availableDevices.Any(d => string.Equals(settings.DeviceName, d.Name, StringComparison.OrdinalIgnoreCase) && settings.DeviceIndex == d.Index);
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-            => throw new NotSupportedException();
+        return availableDevices.Any(d => string.Equals(settings.DeviceName, d.Name, StringComparison.OrdinalIgnoreCase) && settings.DeviceIndex == d.Index);
     }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
 }
