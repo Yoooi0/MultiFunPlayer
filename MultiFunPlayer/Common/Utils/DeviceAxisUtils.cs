a9a70eeed6f221a24afbdfb932704c01898e8f08
Yoooi <kamos21@protonmail.com>
2022-12-17 15:45:52 +0100

    Fix script name marked as unnamed when it should match disabled axis

--- MultiFunPlayer/Common/Utils/DeviceAxisUtils.cs
+++ MultiFunPlayer/Common/Utils/DeviceAxisUtils.cs
@@ -1,20 +1,24 @@
+﻿using MultiFunPlayer.UI.Controls.ViewModels;
 using System.IO;
 
 namespace MultiFunPlayer.Common;
 
 public static class DeviceAxisUtils
 {
+    public static IEnumerable<string> KnownFunscriptNames { get; } = DeviceSettingsViewModel.DefaultDevices.SelectMany(d => d.Axes)
+                                                                                                           .SelectMany(a => a.FunscriptNames)
+                                                                                                           .Distinct()
+                                                                                                           .ToList();
+
     public static IEnumerable<DeviceAxis> FindAxesMatchingName(string scriptName) => FindAxesMatchingName(DeviceAxis.All, scriptName);
-    public static IEnumerable<DeviceAxis> FindAxesMatchingName(string scriptName, string mediaName) => FindAxesMatchingName(DeviceAxis.All, scriptName, mediaName);
     public static IEnumerable<DeviceAxis> FindAxesMatchingName(IEnumerable<DeviceAxis> axes, string scriptName)
     {
         var scriptWithoutExtension = Path.GetFileNameWithoutExtension(scriptName);
-        var isUnnamedScript = !DeviceAxis.All.SelectMany(a => a.FunscriptNames)
-                                             .Distinct()
-                                             .Any(n => scriptWithoutExtension.EndsWith(n, StringComparison.OrdinalIgnoreCase));
+        var isUnnamedScript = !KnownFunscriptNames.Any(n => scriptWithoutExtension.EndsWith(n, StringComparison.OrdinalIgnoreCase));
         return FindAxesMatchingName(axes, scriptName, isUnnamedScript);
     }
 
+    public static IEnumerable<DeviceAxis> FindAxesMatchingName(string scriptName, string mediaName) => FindAxesMatchingName(DeviceAxis.All, scriptName, mediaName);
     public static IEnumerable<DeviceAxis> FindAxesMatchingName(IEnumerable<DeviceAxis> axes, string scriptName, string mediaName)
     {
         var scriptWithoutExtension = Path.GetFileNameWithoutExtension(scriptName);
@@ -31,7 +35,7 @@ public static class DeviceAxisUtils
         {
             if (isUnnamedScript)
                 if (axis.LoadUnnamedScript)
-                yield return axis;
+                    yield return axis;
             else if (axis.FunscriptNames.Any(n => scriptWithoutExtension.EndsWith(n, StringComparison.OrdinalIgnoreCase)))
                 yield return axis;
         }
