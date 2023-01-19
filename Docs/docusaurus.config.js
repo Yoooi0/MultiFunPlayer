01ba55d12fea2fc2ef8d1d0ee17cec600d1ce20d
Yoooi <kamos21@protonmail.com>
2023-01-20 00:44:50 +0100

    Fix docs

--- Docs/docusaurus.config.js
+++ Docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://Yoooi0.github.io',
-  baseUrl: '/',
+  baseUrl: '/MultiFunPlayer/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
