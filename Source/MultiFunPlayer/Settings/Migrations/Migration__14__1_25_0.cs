f5ed08a4bcd3f708b35912c376a89bed8fd3f2e9
Yoooi <kamos21@protonmail.com>
2023-02-24 20:13:26 +0100

    Add Buttplug.Net migration (#115)

---
+++ Source/MultiFunPlayer/Settings/Migrations/Migration__14__1_25_0.cs
@@ -0,0 +1,42 @@
+﻿using MultiFunPlayer.Common;
+using Newtonsoft.Json.Linq;
+using NLog;
+
+namespace MultiFunPlayer.Settings.Migrations;
+
+internal class Migration__14__1_25_0 : AbstractConfigMigration
+{
+    private readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    public override void Migrate(JObject settings)
+    {
+        if (settings.TryGetObject(out var outputTargetSettings, "OutputTarget"))
+            MigrateButtplugOutputSettings(outputTargetSettings);
+
+        base.Migrate(settings);
+    }
+
+    private void MigrateButtplugOutputSettings(JObject settings)
+    {
+        Logger.Info("Migrating ButtplugOutputTarget");
+
+        foreach (var deviceSettings in settings.SelectTokens("$.Items[?(@.$type =~ /.*ButtplugOutputTargetViewModel.*/)].DeviceSettings[*]").OfType<JObject>())
+        {
+            deviceSettings.RenameProperty("FeatureIndex", "ActuatorIndex");
+            Logger.Info("Migrated property from \"FeatureIndex\" to \"ActuatorIndex\"");
+
+            var messageType = deviceSettings["MessageType"];
+            var actuatorType = messageType.ToString() switch
+            {
+                "VibrateCmd" => "Vibrate",
+                "RotateCmd" => "Rotate",
+                "LinearCmd" => "Position",
+                _ => "Unknown"
+            };
+
+            deviceSettings.Remove("MessageType");
+            deviceSettings.Add("ActuatorType", JToken.FromObject(actuatorType));
+            Logger.Info($"Migrated \"MessageType={messageType}\" to \"ActuatorType={actuatorType}\"");
+        }
+    }
+}
