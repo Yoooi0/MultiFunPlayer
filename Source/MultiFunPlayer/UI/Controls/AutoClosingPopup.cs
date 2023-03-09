04e54935ae461f499ef24236c13cb1b7d5d5fd53
Yoooi <kamos21@protonmail.com>
2023-03-09 21:44:19 +0100

    Force close heatmap tooltip on mouse event

---
+++ Source/MultiFunPlayer/UI/Controls/AutoClosingPopup.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace MultiFunPlayer.UI.Controls;
+
+internal class AutoClosingPopup : Popup
+{
+    public AutoClosingPopup() => IsHitTestVisible = true;
+
+    protected override void OnMouseEnter(MouseEventArgs e) => Close(e);
+    protected override void OnMouseLeave(MouseEventArgs e) => Close(e);
+    protected override void OnMouseMove(MouseEventArgs e) => Close(e);
+    protected override void OnPreviewMouseMove(MouseEventArgs e) => Close(e);
+
+    private void Close(MouseEventArgs e)
+    {
+        e.Handled = true;
+        IsOpen = false;
+    }
+}
