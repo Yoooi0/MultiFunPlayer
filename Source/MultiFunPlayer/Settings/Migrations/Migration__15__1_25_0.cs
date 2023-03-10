f001ed462a2d22bf1fc3514fb6ec6d7f5a157b05
Yoooi <kamos21@protonmail.com>
2023-03-10 20:20:13 +0100

    Migrate bypass settings/actions (#71)

---
+++ Source/MultiFunPlayer/Settings/Migrations/Migration__15__1_25_0.cs
@@ -0,0 +1,51 @@
+﻿using MultiFunPlayer.Common;
+using Newtonsoft.Json.Linq;
+using NLog;
+
+namespace MultiFunPlayer.Settings.Migrations;
+
+internal class Migration__15__1_25_0 : AbstractConfigMigration
+{
+    private readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    public override void Migrate(JObject settings)
+    {
+        MigrateBypassAxisSettings(settings);
+
+        if (settings.TryGetObject(out var shortcutSettings, "Shortcuts"))
+            MigrateBypassActions(shortcutSettings);
+
+        base.Migrate(settings);
+    }
+
+    private void MigrateBypassAxisSettings(JObject settings)
+    {
+        Logger.Info("Migrating bypass settings");
+
+        foreach (var axisSettings in settings.SelectTokens("$.Script.AxisSettings.*").OfType<JObject>())
+        {
+            if (!axisSettings.TryGetValue<bool>("Bypass", out var bypass))
+                continue;
+
+            axisSettings.Remove("Bypass");
+            axisSettings.Add("BypassScript", JToken.FromObject(bypass));
+            axisSettings.Add("BypassMotionProvider", JToken.FromObject(bypass));
+            axisSettings.Add("BypassTransition", JToken.FromObject(bypass));
+
+            Logger.Info($"Migrated \"Bypass={bypass}\" to \"BypassScript={bypass}\", \"BypassMotionProvider={bypass}\", \"BypassTransition={bypass}\"");
+        }
+    }
+
+    private void MigrateBypassActions(JObject settings)
+    {
+        Logger.Info("Migrating bypass actions");
+        foreach (var action in settings.SelectTokens("$.Bindings[*].Actions[?(@.Descriptor =~ /Axis::Bypass::.*/i)]").OfType<JObject>())
+        {
+            var oldDescriptor = action["Descriptor"].ToString();
+            var newDescriptor = oldDescriptor.Replace("Axis::Bypass::", "Axis::Bypass::All::");
+
+            action["Descriptor"] = newDescriptor;
+            Logger.Info("Migrated action descriptor from \"{0}\" to \"{1}\"", oldDescriptor, newDescriptor);
+        }
+    }
+}
