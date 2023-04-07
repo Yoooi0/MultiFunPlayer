0874831d57a694e2904dcbae844597d661b67b22
Yoooi <kamos21@protonmail.com>
2023-04-08 01:12:34 +0200

    Migrate all bindings as enabled (#120)

---
+++ Source/MultiFunPlayer/Settings/Migrations/Migration__16__1_25_0.cs
@@ -0,0 +1,34 @@
+﻿using MultiFunPlayer.Common;
+using Newtonsoft.Json.Linq;
+using NLog;
+
+namespace MultiFunPlayer.Settings.Migrations;
+
+internal class Migration__16__1_25_0 : AbstractConfigMigration
+{
+    private readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    public override void Migrate(JObject settings)
+    {
+        if (settings.TryGetObject(out var shortcutSettings, "Shortcuts"))
+            MigrateBindings(shortcutSettings);
+
+        base.Migrate(settings);
+    }
+
+    private void MigrateBindings(JObject settings)
+    {
+        Logger.Info("Migrating bindings");
+        if (!settings.ContainsKey("Bindings"))
+            return;
+
+        foreach (var binding in settings["Bindings"].Children<JObject>())
+        {
+            if (binding.ContainsKey("Enabled"))
+                continue;
+
+            binding.Add("Enabled", JToken.FromObject(true));
+            Logger.Info("Added \"Enabled=true\" property to binding");
+        }
+    }
+}
