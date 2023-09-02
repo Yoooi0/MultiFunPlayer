a28dfd9d172a6f823514331830a2497928e8a82e
Yoooi <kamos21@protonmail.com>
2023-09-02 23:40:20 +0200

    Add VLC source (#135)

---
+++ Source/MultiFunPlayer/MediaSource/ViewModels/VlcMediaSourceViewModel.cs
@@ -0,0 +1,342 @@
+using MultiFunPlayer.Common;
+using MultiFunPlayer.Input;
+using MultiFunPlayer.UI;
+using Newtonsoft.Json.Linq;
+using NLog;
+using Stylet;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Channels;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace MultiFunPlayer.MediaSource.ViewModels;
+
+[DisplayName("VLC")]
+internal class VlcMediaSourceViewModel : AbstractMediaSource, IHandle<MediaPlayPauseMessage>, IHandle<MediaSeekMessage>, IHandle<MediaChangePathMessage>
+{
+    private static Logger Logger { get; } = LogManager.GetCurrentClassLogger();
+
+    private readonly Channel<object> _writeMessageChannel;
+    private PlayerState _playerState;
+
+    public override ConnectionStatus Status { get; protected set; }
+
+    public EndPoint Endpoint { get; set; } = new IPEndPoint(IPAddress.Loopback, 8080);
+    public string Password { get; set; } = null;
+
+    public VlcMediaSourceViewModel(IShortcutManager shortcutManager, IEventAggregator eventAggregator)
+        : base(shortcutManager, eventAggregator)
+    {
+        _writeMessageChannel = Channel.CreateUnbounded<object>(new UnboundedChannelOptions()
+        {
+            SingleReader = true,
+            SingleWriter = true,
+        });
+    }
+
+    public bool IsConnected => Status == ConnectionStatus.Connected;
+    public bool IsConnectBusy => Status == ConnectionStatus.Connecting || Status == ConnectionStatus.Disconnecting;
+    public bool CanToggleConnect => !IsConnectBusy;
+
+    protected override async Task RunAsync(CancellationToken token)
+    {
+        try
+        {
+            Logger.Info("Connecting to {0} at \"{1}\"", Name, Endpoint);
+            if (Endpoint == null)
+                throw new Exception("Endpoint cannot be null.");
+
+            using var client = NetUtils.CreateHttpClient();
+            client.Timeout = TimeSpan.FromMilliseconds(1000);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($":{Password}")));
+
+            var uri = new Uri($"http://{Endpoint.ToUriString()}/requests/status.xml");
+            var response = await UnwrapTimeout(() => client.GetAsync(uri, token));
+            response.EnsureSuccessStatusCode();
+
+            Status = ConnectionStatus.Connected;
+            while (_writeMessageChannel.Reader.TryRead(out _));
+
+            _playerState = new PlayerState();
+
+            using var cancellationSource = CancellationTokenSource.CreateLinkedTokenSource(token);
+            var task = await Task.WhenAny(ReadAsync(client, cancellationSource.Token), WriteAsync(client, cancellationSource.Token));
+            cancellationSource.Cancel();
+
+            task.ThrowIfFaulted();
+        }
+        catch (OperationCanceledException) { }
+        catch (Exception e)
+        {
+            Logger.Error(e, $"{Name} failed with exception");
+            _ = DialogHelper.ShowErrorAsync(e, $"{Name} failed with exception", "RootDialog");
+        }
+
+        if (IsDisposing)
+            return;
+
+        EventAggregator.Publish(new MediaPathChangedMessage(null));
+        EventAggregator.Publish(new MediaPlayingChangedMessage(false));
+    }
+
+    private async Task ReadAsync(HttpClient client, CancellationToken token)
+    {
+        var statusUri = new Uri($"http://{Endpoint.ToUriString()}/requests/status.xml");
+        var playlistUri = new Uri($"http://{Endpoint.ToUriString()}/requests/playlist.xml");
+
+        try
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Delay(200, token);
+
+                var statusResponse = await UnwrapTimeout(() => client.GetAsync(statusUri, token));
+                if (statusResponse == null)
+                    continue;
+
+                statusResponse.EnsureSuccessStatusCode();
+                var statusMessage = await statusResponse.Content.ReadAsStringAsync(token);
+
+                Logger.Trace("Received \"{0}\" from \"{1}\"", statusMessage, Name);
+                var statusDocument = XDocument.Parse(statusMessage);
+
+                if (!int.TryParse(statusDocument.Root.Element("currentplid")?.Value, out var playlistId))
+                    continue;
+
+                var lastPlaylistId = _playerState.PlaylistId;
+                if (playlistId < 0)
+                {
+                    ResetState();
+                    continue;
+                }
+
+                if (playlistId != lastPlaylistId)
+                {
+                    var playlistResponse = await UnwrapTimeout(() => client.GetAsync(playlistUri, token));
+                    if (playlistResponse == null)
+                        continue;
+
+                    playlistResponse.EnsureSuccessStatusCode();
+                    var playlistMessage = await playlistResponse.Content.ReadAsStringAsync(token);
+
+                    Logger.Trace("Received \"{0}\" from \"{1}\"", playlistMessage, Name);
+
+                    var playlistDocument = XDocument.Parse(playlistMessage);
+                    var element = playlistDocument.XPathSelectElement($".//leaf[@id={playlistId}]");
+                    var path = element?.Attribute("uri")?.Value;
+
+                    if (string.IsNullOrWhiteSpace(path))
+                    {
+                        ResetState();
+                        continue;
+                    }
+
+                    EventAggregator.Publish(new MediaPathChangedMessage(Uri.UnescapeDataString(path)));
+                    _playerState.PlaylistId = playlistId;
+                }
+
+                if (statusDocument.Root.Element("state")?.Value is string state && state != _playerState.State)
+                {
+                    EventAggregator.Publish(new MediaPlayingChangedMessage(string.Equals(state, "playing", StringComparison.OrdinalIgnoreCase)));
+                    _playerState.State = state;
+                }
+
+                if (int.TryParse(statusDocument.Root.Element("length")?.Value, out var duration) && duration >= 0 && duration != _playerState.Duration)
+                {
+                    EventAggregator.Publish(new MediaDurationChangedMessage(TimeSpan.FromSeconds(duration)));
+                    _playerState.Duration = duration;
+                }
+
+                if (double.TryParse(statusDocument.Root.Element("position")?.Value, out var position) && position >= 0 && position != _playerState.Position && _playerState.Duration != null)
+                {
+                    EventAggregator.Publish(new MediaPositionChangedMessage(TimeSpan.FromSeconds(position * (double)_playerState.Duration)));
+                    _playerState.Position = position;
+                }
+
+                if (double.TryParse(statusDocument.Root.Element("rate")?.Value, out var speed) && speed > 0 && speed != _playerState.Speed)
+                {
+                    EventAggregator.Publish(new MediaSpeedChangedMessage(speed));
+                    _playerState.Speed = speed;
+                }
+            }
+        }
+        catch (OperationCanceledException) { }
+
+        void ResetState()
+        {
+            _playerState = new PlayerState();
+
+            EventAggregator.Publish(new MediaPathChangedMessage(null));
+            EventAggregator.Publish(new MediaPlayingChangedMessage(false));
+        }
+    }
+
+    private async Task WriteAsync(HttpClient client, CancellationToken token)
+    {
+        var uriBase = $"http://{Endpoint.ToUriString()}/requests/status.xml";
+
+        try
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await _writeMessageChannel.Reader.WaitToReadAsync(token);
+                var message = await _writeMessageChannel.Reader.ReadAsync(token);
+
+                var isPlaying = string.Equals(_playerState?.State, "playing", StringComparison.OrdinalIgnoreCase);
+                var uriArguments = message switch
+                {
+                    MediaPlayPauseMessage playPauseMessage when isPlaying != playPauseMessage.ShouldBePlaying => "pl_pause",
+                    MediaSeekMessage seekMessage => $"seek&val={(int)seekMessage.Position.TotalSeconds}",
+                    MediaChangePathMessage changePathMessage => string.IsNullOrWhiteSpace(changePathMessage.Path) ? "pl_stop" : $"in_play&input={Uri.EscapeDataString(changePathMessage.Path)}",
+                    _ => null
+                };
+
+                if (uriArguments == null)
+                    continue;
+
+                var requestUri = new Uri($"{uriBase}?command={uriArguments}");
+                Logger.Trace("Sending \"{0}\" to \"{1}\"", uriArguments, Name);
+
+                var response = await UnwrapTimeout(() => client.GetAsync(requestUri, token));
+                response.EnsureSuccessStatusCode();
+            }
+        }
+        catch (OperationCanceledException) { }
+    }
+
+    public override void HandleSettings(JObject settings, SettingsAction action)
+    {
+        base.HandleSettings(settings, action);
+
+        if (action == SettingsAction.Saving)
+        {
+            settings[nameof(Endpoint)] = Endpoint?.ToString();
+
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(Password))
+                {
+                    var encryptedPassword = Convert.ToBase64String(ProtectedData.Protect(Encoding.UTF8.GetBytes(Password), null, DataProtectionScope.CurrentUser));
+                    settings[nameof(Password)] = JToken.FromObject(encryptedPassword);
+                }
+                else
+                {
+                    settings[nameof(Password)] = null;
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Warn(e, "Failed to encrypt password to settings");
+            }
+        }
+        else if (action == SettingsAction.Loading)
+        {
+            if (settings.TryGetValue<EndPoint>(nameof(Endpoint), out var endpoint))
+                Endpoint = endpoint;
+
+            if (settings.TryGetValue<string>(nameof(Password), out var encryptedPassword))
+            {
+                try
+                {
+                    Password = Encoding.UTF8.GetString(ProtectedData.Unprotect(Convert.FromBase64String(encryptedPassword), null, DataProtectionScope.CurrentUser));
+                }
+                catch (Exception e)
+                {
+                    Logger.Warn(e, "Failed to decrypt password from settings");
+                }
+            }
+        }
+    }
+
+    public override async ValueTask<bool> CanConnectAsync(CancellationToken token)
+    {
+        try
+        {
+            if (Endpoint == null)
+                return false;
+
+            var uri = new Uri($"http://{Endpoint.ToUriString()}");
+
+            using var client = NetUtils.CreateHttpClient();
+            client.Timeout = TimeSpan.FromMilliseconds(50);
+
+            var response = await client.GetAsync(uri, token);
+            response.EnsureSuccessStatusCode();
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private async Task<HttpResponseMessage> UnwrapTimeout(Func<Task<HttpResponseMessage>> action)
+    {
+        //https://github.com/dotnet/runtime/issues/21965
+
+        try
+        {
+            return await action();
+        }
+        catch (Exception e)
+        {
+            if (e is OperationCanceledException operationCanceledException)
+            {
+                var innerException = operationCanceledException.InnerException;
+                if (innerException is TimeoutException)
+                    innerException.Throw();
+
+                operationCanceledException.Throw();
+            }
+
+            e.Throw();
+            return null;
+        }
+    }
+
+    protected override void RegisterActions(IShortcutManager s)
+    {
+        base.RegisterActions(s);
+
+        #region Endpoint
+        s.RegisterAction<string>($"{Name}::Endpoint::Set", s => s.WithLabel("Endpoint").WithDescription("ip/host:port"), endpointString =>
+        {
+            if (NetUtils.TryParseEndpoint(endpointString, out var endpoint))
+                Endpoint = endpoint;
+        });
+        #endregion
+    }
+
+    public async void Handle(MediaSeekMessage message)
+    {
+        if (Status == ConnectionStatus.Connected)
+            await _writeMessageChannel.Writer.WriteAsync(message);
+    }
+
+    public async void Handle(MediaPlayPauseMessage message)
+    {
+        if (Status == ConnectionStatus.Connected)
+            await _writeMessageChannel.Writer.WriteAsync(message);
+    }
+
+    public async void Handle(MediaChangePathMessage message)
+    {
+        if (Status == ConnectionStatus.Connected)
+            await _writeMessageChannel.Writer.WriteAsync(message);
+    }
+
+    private class PlayerState
+    {
+        public double? Position { get; set; }
+        public double? Speed { get; set; }
+        public string State { get; set; }
+        public int? Duration { get; set; }
+        public int? PlaylistId { get; set; }
+    }
+}
---
+++ Source/MultiFunPlayer/UI/Behaviours/PasswordBoxAssist.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MultiFunPlayer.UI.Behaviours;
+
+public static class PasswordBoxAssist
+{
+    private static bool _eventAttached;
+    private static bool _ignorePasswordChanged;
+
+    public static readonly DependencyProperty PasswordProperty =
+        DependencyProperty.RegisterAttached("Password",
+            typeof(string), typeof(PasswordBoxAssist),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    OnPasswordPropertyChanged, OnCoercePasswordPropertyChanged));
+
+    private static object OnCoercePasswordPropertyChanged(DependencyObject dp, object baseValue)
+    {
+        if (dp is PasswordBox passwordBox && !_eventAttached)
+            AttachEvent(passwordBox);
+
+        return baseValue;
+    }
+
+    public static string GetPassword(DependencyObject dp)
+        => (string)dp.GetValue(PasswordProperty);
+
+    public static void SetPassword(DependencyObject dp, string value)
+        => dp.SetValue(PasswordProperty, value);
+
+    private static void OnPasswordPropertyChanged(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+    {
+        if (_ignorePasswordChanged)
+            return;
+
+        if (dp is not PasswordBox passwordBox)
+            return;
+
+        if (!_eventAttached)
+            AttachEvent(passwordBox);
+
+        if (e.NewValue is not string value)
+            return;
+
+        passwordBox.Password = value;
+    }
+
+    private static void AttachEvent(PasswordBox passwordBox)
+    {
+        passwordBox.PasswordChanged -= OnPasswordChanged;
+        passwordBox.PasswordChanged += OnPasswordChanged;
+        _eventAttached = true;
+    }
+
+    private static void OnPasswordChanged(object sender, RoutedEventArgs e)
+    {
+        if (sender is not PasswordBox passwordBox)
+            return;
+
+        _ignorePasswordChanged = true;
+        SetPassword(sender as DependencyObject, passwordBox.Password);
+        _ignorePasswordChanged = false;
+    }
+}
