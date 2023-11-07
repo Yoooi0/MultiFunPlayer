329b12a7beef789bdda461eb935164116cf6875c
Yoooi <kamos21@protonmail.com>
2023-11-07 01:28:09 +0100

    Create TCode button gesture only on button release

--- Source/MultiFunPlayer/Input/TCode/TCodeButtonGesture.cs
+++ Source/MultiFunPlayer/Input/TCode/TCodeButtonGesture.cs
@@ -9,17 +9,12 @@ public class TCodeButtonGesture : ISimpleInputGesture
 {
     private readonly TCodeButtonGestureDescriptor _descriptor;
 
-    public bool State { get; }
     public string Button => _descriptor.Button;
     public IInputGestureDescriptor Descriptor => _descriptor;
 
-    public TCodeButtonGesture(TCodeButtonGestureDescriptor descriptor, bool state)
-    {
-        State = state;
-        _descriptor = descriptor;
-    }
+    public TCodeButtonGesture(TCodeButtonGestureDescriptor descriptor) => _descriptor = descriptor;
 
     public override string ToString() => $"[TCode Button: {Button}]";
 
-    public static TCodeButtonGesture Create(string button, bool state) => new(new(button), state);
+    public static TCodeButtonGesture Create(string button, bool state) => new(new(button));
 }
--- Source/MultiFunPlayer/Input/TCode/TCodeInputProcessor.cs
+++ Source/MultiFunPlayer/Input/TCode/TCodeInputProcessor.cs
@@ -6,18 +6,26 @@ namespace MultiFunPlayer.Input.TCode;
 internal class TCodeInputProcessor : IInputProcessor
 {
     protected static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    private readonly Dictionary<string, bool> _buttonStates;
 
     public event EventHandler<IInputGesture> OnGesture;
+
+    public TCodeInputProcessor()
+    {
+        _buttonStates = new Dictionary<string, bool>();
+    }
+
     public void Parse(string input)
     {
-        Logger.Debug("Input: {1}", input);
-
         var buttonMatch = Regex.Match(input, "#(.+?):(0|1)");
         if (buttonMatch.Success)
         {
             var button = buttonMatch.Groups[1].Value;
             var state = int.Parse(buttonMatch.Groups[2].Value) == 1;
-            HandleGesture(TCodeButtonGesture.Create(button, state));
+            if (_buttonStates.ContainsKey(button) && !state && _buttonStates[button])
+                HandleGesture(TCodeButtonGesture.Create(button, state));
+
+            _buttonStates[button] = state;
         }
     }
 
