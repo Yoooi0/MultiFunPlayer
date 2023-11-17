f5357c68855bb9fe3a0ae36736e5f2da18abf8fa
Yoooi <kamos21@protonmail.com>
2023-11-17 03:35:20 +0100

    Cleanup

--- Source/MultiFunPlayer/Common/Messages.cs
+++ Source/MultiFunPlayer/Common/Messages.cs
@@ -18,12 +18,10 @@ public record MediaPathChangedMessage(string Path, bool ReloadScripts = true);
 public record MediaDurationChangedMessage(TimeSpan? Duration);
 
 public record ScriptChangedMessage(DeviceAxis Axis, IScriptResource Script);
-public class ChangeScriptMessage
+public record class ChangeScriptMessage(Dictionary<DeviceAxis, IScriptResource> Scripts)
 {
-    public Dictionary<DeviceAxis, IScriptResource> Scripts { get; }
-    public ChangeScriptMessage(Dictionary<DeviceAxis, IScriptResource> scripts) => Scripts = scripts;
-    public ChangeScriptMessage(DeviceAxis axis, IScriptResource script) => Scripts = new() { [axis] = script };
-    public ChangeScriptMessage(IEnumerable<DeviceAxis> axes, IScriptResource scriptResource) => Scripts = axes.ToDictionary(a => a, _ => scriptResource);
+    public ChangeScriptMessage(DeviceAxis axis, IScriptResource script) : this(new Dictionary<DeviceAxis, IScriptResource>() { [axis] = script }) { }
+    public ChangeScriptMessage(IEnumerable<DeviceAxis> axes, IScriptResource scriptResource) : this(axes.ToDictionary(a => a, _ => scriptResource)) { }
 }
 
 public interface IMediaSourceControlMessage { }
@@ -32,11 +30,8 @@ public record MediaPlayPauseMessage(bool ShouldBePlaying) : IMediaSourceControlM
 public record MediaChangePathMessage(string Path) : IMediaSourceControlMessage;
 public record MediaChangeSpeedMessage(double Speed) : IMediaSourceControlMessage;
 
-public class SyncRequestMessage
+public record class SyncRequestMessage(List<DeviceAxis> Axes = null)
 {
-    public List<DeviceAxis> Axes { get; }
-
-    public SyncRequestMessage() => Axes = null;
     public SyncRequestMessage(params DeviceAxis[] axes) : this(axes?.AsEnumerable()) { }
-    public SyncRequestMessage(IEnumerable<DeviceAxis> axes) => Axes = axes?.ToList();
+    public SyncRequestMessage(IEnumerable<DeviceAxis> axes) : this(axes?.ToList()) { }
 }
