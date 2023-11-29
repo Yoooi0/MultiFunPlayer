3b89ed8eb0ef7682c8d091d142699b919bf1ef7c
Yoooi <kamos21@protonmail.com>
2023-11-29 19:12:19 +0100

    Cleanup

--- Source/MultiFunPlayer/Common/Utils/MathUtils.cs
+++ Source/MultiFunPlayer/Common/Utils/MathUtils.cs
@@ -8,22 +8,13 @@ public static class MathUtils
     public static double Clamp01(double x) => Math.Clamp(x, 0, 1);
 
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-    public static double Lerp(double from, double to, double t) => LerpUnclamped(from, to, Math.Clamp(t, 0, 1));
+    public static double Lerp(double from, double to, double t) => from + (to - from) * Math.Clamp(t, 0, 1);
 
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-    public static double LerpUnclamped(double from, double to, double t) => from + (to - from) * t;
-
-    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-    public static double UnLerp(double from, double to, double t) => Clamp01(UnLerpUnclamped(from, to, t));
-
-    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-    public static double UnLerpUnclamped(double from, double to, double t) => (t - from) / (to - from);
+    public static double UnLerp(double from, double to, double t) => Math.Clamp((t - from) / (to - from), 0, 1);
 
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     public static double Map(double x, double from0, double to0, double from1, double to1) => Lerp(from1, to1, UnLerp(from0, to0, x));
-
-    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-    public static double MapUnclamped(double x, double from0, double to0, double from1, double to1) => LerpUnclamped(from1, to1, UnLerpUnclamped(from0, to0, x));
 }
 
 public enum InterpolationType
