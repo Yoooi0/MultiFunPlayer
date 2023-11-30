7044f8cf6faa939c6fe9be86be7466f592788713
Yoooi <kamos21@protonmail.com>
2023-11-30 17:24:09 +0100

    Cleanup

--- Source/MultiFunPlayer/Input/XInput/XInputProcessor.cs
+++ Source/MultiFunPlayer/Input/XInput/XInputProcessor.cs
@@ -92,26 +92,32 @@ internal sealed class XInputProcessor : IInputProcessor
     {
         void CreateAxisGestureShort(short last, short current, GamepadAxis axis)
         {
-            var delta = (double)(current - last) / ushort.MaxValue;
-            var value = MathUtils.Map(current, short.MinValue, short.MaxValue, 0, 1);
+            if (current != last)
+                return;
+
+            var delta = MathUtils.UnLerp(-ushort.MaxValue, ushort.MaxValue, current - last);
+            var value = MathUtils.UnLerp(short.MinValue, short.MaxValue, current);
             HandleGesture(GamepadAxisGesture.Create(userIndex, axis, value, delta, elapsed));
         }
 
         void CreateAxisGestureByte(byte last, byte current, GamepadAxis axis)
         {
-            var delta = (double)(current - last) / byte.MaxValue;
-            var value = MathUtils.Map(current, byte.MinValue, byte.MaxValue, 0, 1);
+            if (current != last)
+                return;
+
+            var delta = MathUtils.UnLerp(-byte.MaxValue, byte.MaxValue, current - last);
+            var value = MathUtils.UnLerp(byte.MinValue, byte.MaxValue, current);
             HandleGesture(GamepadAxisGesture.Create(userIndex, axis, value, delta, elapsed));
         }
 
-        if (current.RightThumbX != last.RightThumbX) CreateAxisGestureShort(last.RightThumbX, current.RightThumbX, GamepadAxis.RightThumbX);
-        if (current.RightThumbY != last.RightThumbY) CreateAxisGestureShort(last.RightThumbY, current.RightThumbY, GamepadAxis.RightThumbY);
+        CreateAxisGestureShort(last.RightThumbX, current.RightThumbX, GamepadAxis.RightThumbX);
+        CreateAxisGestureShort(last.RightThumbY, current.RightThumbY, GamepadAxis.RightThumbY);
 
-        if (current.LeftThumbX != last.LeftThumbX) CreateAxisGestureShort(last.LeftThumbX, current.LeftThumbX, GamepadAxis.LeftThumbX);
-        if (current.LeftThumbY != last.LeftThumbY) CreateAxisGestureShort(last.LeftThumbY, current.LeftThumbY, GamepadAxis.LeftThumbY);
+        CreateAxisGestureShort(last.LeftThumbX, current.LeftThumbX, GamepadAxis.LeftThumbX);
+        CreateAxisGestureShort(last.LeftThumbY, current.LeftThumbY, GamepadAxis.LeftThumbY);
 
-        if (current.RightTrigger != last.RightTrigger) CreateAxisGestureByte(last.RightTrigger, current.RightTrigger, GamepadAxis.RightTrigger);
-        if (current.LeftTrigger != last.LeftTrigger) CreateAxisGestureByte(last.LeftTrigger, current.LeftTrigger, GamepadAxis.LeftTrigger);
+        CreateAxisGestureByte(last.RightTrigger, current.RightTrigger, GamepadAxis.RightTrigger);
+        CreateAxisGestureByte(last.LeftTrigger, current.LeftTrigger, GamepadAxis.LeftTrigger);
     }
 
     private void HandleGesture(IInputGesture gesture)
