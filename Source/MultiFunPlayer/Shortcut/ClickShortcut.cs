e3f12d008872e53d2ecf352843abd0edcd8e7c62
Yoooi <kamos21@protonmail.com>
2024-01-09 01:29:57 +0100

    Fix click shortcuts invoking with more clicks than configured

--- Source/MultiFunPlayer/Shortcut/ClickShortcut.cs
+++ Source/MultiFunPlayer/Shortcut/ClickShortcut.cs
@@ -8,48 +8,26 @@ internal sealed class ClickShortcut(IShortcutActionResolver actionResolver, ISim
     : AbstractShortcut<ISimpleInputGesture, ISimpleInputGestureData>(actionResolver, gesture)
 {
     private int _stateCounter;
-    private int _lastClickTime;
 
     public int ClickCount { get; set; } = 2;
     public int MaximumClickInterval { get; set; } = 200;
 
     protected override void Update(ISimpleInputGesture gesture)
     {
-        if (gesture.State && _stateCounter == 0)
+        if (gesture.State && _stateCounter % 2 == 0)
         {
             _stateCounter++;
+            CancelTask();
         }
-        else if (gesture.State && _stateCounter > 0)
+        else if (!gesture.State && _stateCounter % 2 == 1)
         {
-            if (_stateCounter % 2 == 1) //consecutive press
-                return;
-
             _stateCounter++;
-            if (Environment.TickCount - _lastClickTime > MaximumClickInterval)
-            {
-                ResetState();
-                return;
-            }
-        }
-        else if (!gesture.State && _stateCounter > 0)
-        {
-            if (_stateCounter % 2 == 0) //consecutive release
-                return;
+            ScheduleTask(MaximumClickInterval, () => {
+                if (_stateCounter == 2 * ClickCount)
+                    Invoke(SimpleInputGestureData.FromGesture(gesture));
 
-            _stateCounter++;
-            _lastClickTime = Environment.TickCount;
+                _stateCounter = 0;
+            });
         }
-
-        if (_stateCounter == 2 * ClickCount)
-        {
-            ResetState();
-            Invoke(SimpleInputGestureData.FromGesture(gesture));
-        }
-    }
-
-    private void ResetState()
-    {
-        _stateCounter = 0;
-        _lastClickTime = 0;
     }
 }
