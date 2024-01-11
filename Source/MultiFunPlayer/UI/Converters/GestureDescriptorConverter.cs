0783969e97be82d1e3462745f55fc0b79220b9ac
Yoooi <kamos21@protonmail.com>
2024-01-11 16:03:39 +0100

    Rework shortcut settings UI

---
+++ Source/MultiFunPlayer/UI/Converters/GestureDescriptorConverter.cs
@@ -0,0 +1,59 @@
+﻿using MaterialDesignThemes.Wpf;
+using MultiFunPlayer.Input.RawInput;
+using MultiFunPlayer.Input.TCode;
+using MultiFunPlayer.Input.XInput;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MultiFunPlayer.UI.Converters;
+
+internal sealed class GestureDescriptorToBrushConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string || value == null)
+            return null;
+
+        var colorHex = value switch
+        {
+            KeyboardGestureDescriptor => "#BD6ECA",
+            MouseAxisGestureDescriptor => "#81C784",
+            MouseButtonGestureDescriptor => "#81C784",
+            TCodeAxisGestureDescriptor => "#D8494C",
+            TCodeButtonGestureDescriptor => "#D8494C",
+            GamepadAxisGestureDescriptor => "#F4B800",
+            GamepadButtonGestureDescriptor => "#F4B800",
+            _ => throw new NotImplementedException()
+        };
+
+        return new SolidColorBrush((Color)ColorConverter.ConvertFromString(colorHex));
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
+
+internal sealed class GestureDescriptorToPackIconConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string || value == null)
+            return null;
+
+        return value switch
+        {
+            KeyboardGestureDescriptor => PackIconKind.KeyboardOutline,
+            MouseAxisGestureDescriptor => PackIconKind.MouseMoveVertical,
+            MouseButtonGestureDescriptor => PackIconKind.Mouse,
+            TCodeAxisGestureDescriptor => PackIconKind.AlphaTBoxOutline,
+            TCodeButtonGestureDescriptor => PackIconKind.AlphaTBoxOutline,
+            GamepadAxisGestureDescriptor => PackIconKind.MicrosoftXboxController,
+            GamepadButtonGestureDescriptor => PackIconKind.MicrosoftXboxController,
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
