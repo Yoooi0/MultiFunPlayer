765484f5b5dd66f0fbd9a4e4256b8c20189b87f8
Yoooi <kamos21@protonmail.com>
2024-01-20 14:25:48 +0100

    Implement view for update contexts in all output targets

---
+++ Source/MultiFunPlayer/UI/Converters/MultiDictionaryKeyToValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+
+namespace MultiFunPlayer.UI.Converters;
+
+internal sealed class MultiDictionaryKeyToValueConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values == null || values.Length != 2)
+            throw new NotSupportedException();
+
+        if (!values[0].GetType().GetInterfaces().Any(i => i.GetGenericTypeDefinition().IsAssignableTo(typeof(IDictionary<,>))))
+            throw new NotSupportedException();
+
+        var method = values[0].GetType().GetMethod("get_Item");
+        return method.Invoke(values[0], [values[1]]);
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
