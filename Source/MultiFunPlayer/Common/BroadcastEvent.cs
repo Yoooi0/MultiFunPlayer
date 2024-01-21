59167d44b2c146ea07f72c80e15739c4da1e6aaf
Yoooi <kamos21@protonmail.com>
2024-01-21 01:53:50 +0100

    Reuse BroadcastEvent WaitOneAsync tasks

--- Source/MultiFunPlayer/Common/BroadcastEvent.cs
+++ Source/MultiFunPlayer/Common/BroadcastEvent.cs
@@ -1,103 +1,95 @@
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
 namespace MultiFunPlayer.Common;
 
 internal sealed class BroadcastEvent<T> where T : class
 {
-    private readonly object _syncRoot = new();
-    private readonly Dictionary<object, ManualResetEvent> _waitHandles = [];
+    private readonly ConcurrentDictionary<object, ManualResetEvent> _waitHandles = [];
+    private readonly ConcurrentDictionary<object, Task<bool>> _tasks = [];
     private volatile T _value;
 
     public void Set(T value)
     {
-        lock (_syncRoot)
-        {
-            _value = value;
-            foreach (var (_, waitHandle) in _waitHandles)
-                waitHandle.Set();
-        }
+        _value = value;
+        foreach (var (_, waitHandle) in _waitHandles)
+            waitHandle.Set();
     }
 
     public void RegisterContext(object context)
     {
-        lock (_syncRoot)
-        {
-            if (_waitHandles.ContainsKey(context))
-                throw new InvalidOperationException("Context can only be registered once");
-            _waitHandles[context] = new ManualResetEvent(false);
-        }
+        if (!_waitHandles.TryAdd(context, new ManualResetEvent(false)))
+            throw new InvalidOperationException("Context can only be registered once");
     }
 
     public void UnregisterContext(object context)
     {
-        lock (_syncRoot)
-            if (_waitHandles.Remove(context, out var waitHandle))
-                waitHandle.Dispose();
+        if (_waitHandles.TryRemove(context, out var waitHandle))
+            waitHandle.Dispose();
+        _ = _tasks.TryRemove(context, out var _);
     }
 
     public (bool Success, T Value) WaitOne(object context, CancellationToken cancellationToken)
     {
-        var waitHandle = GetWaitHandle(context);
+        var waitHandle = _waitHandles[context];
         if (!waitHandle.WaitOne(cancellationToken))
             return (false, default);
 
         cancellationToken.ThrowIfCancellationRequested();
-        return (true, GetValueAndUpdateWaitHandle(context));
+        return (true, CleanupAndGetValue(context));
     }
 
     public async ValueTask<(bool Success, T Value)> WaitOneAsync(object context, CancellationToken cancellationToken)
     {
-        var waitHandle = GetWaitHandle(context);
+        var waitHandle = _waitHandles[context];
         if (waitHandle.WaitOne(0))
-            return (true, GetValueAndUpdateWaitHandle(context));
+            return (true, CleanupAndGetValue(context));
 
-        if (!await waitHandle.WaitOneAsync(cancellationToken))
+        if (!await GetWaitHandleTask(context, cancellationToken))
             return (false, default);
 
         cancellationToken.ThrowIfCancellationRequested();
-        return (true, GetValueAndUpdateWaitHandle(context));
+        return (true, CleanupAndGetValue(context));
     }
 
-    private ManualResetEvent GetWaitHandle(object context)
+    private T CleanupAndGetValue(object context)
     {
-        lock (_syncRoot)
-            return _waitHandles[context];
+        if (!_waitHandles.TryRemove(context, out var waitHandle))
+            throw new UnreachableException();
+
+        _ = _tasks.TryRemove(context, out var _);
+        waitHandle.Dispose();
+
+        if (!_waitHandles.TryAdd(context, new ManualResetEvent(false)))
+            throw new UnreachableException();
+
+        return _value;
     }
 
-    private T GetValueAndUpdateWaitHandle(object context)
-    {
-        lock (_syncRoot)
-        {
-            _waitHandles[context].Dispose();
-            _waitHandles[context] = new ManualResetEvent(false);
-            return _value;
-        }
-    }
+    private Task<bool> GetWaitHandleTask(object context, CancellationToken cancellationToken)
+        => _tasks.GetOrAdd(context, c => _waitHandles[c].WaitOneAsync(cancellationToken));
 
     public static (int Index, T Value) WaitAny(BroadcastEvent<T>[] events, object context, CancellationToken cancellationToken)
     {
-        var waitHandles = events.Select(p => p.GetWaitHandle(context)).Append(cancellationToken.WaitHandle).ToArray();
+        var waitHandles = events.Select(e => e._waitHandles[context]).Append(cancellationToken.WaitHandle).ToArray();
         var index = WaitHandle.WaitAny(waitHandles);
 
         cancellationToken.ThrowIfCancellationRequested();
-        return (index, events[index].GetValueAndUpdateWaitHandle(context));
+        return (index, events[index].CleanupAndGetValue(context));
     }
 
     public static async ValueTask<(int Index, T Value)> WaitAnyAsync(BroadcastEvent<T>[] events, object context, CancellationToken cancellationToken)
     {
-        var waitHandles = events.Select(p => p.GetWaitHandle(context)).Append(cancellationToken.WaitHandle).ToArray();
+        var waitHandles = events.Select(e => e._waitHandles[context]).Append(cancellationToken.WaitHandle).ToArray();
         var index = WaitHandle.WaitAny(waitHandles, 0);
         if (index == WaitHandle.WaitTimeout)
         {
-            using var tasksCancellationSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-            var tasksCancellationToken = tasksCancellationSource.Token;
-            var tasks = waitHandles[..^1].Select(h => h.WaitOneAsync(tasksCancellationToken)).ToList();
+            var tasks = events.Select(e => e.GetWaitHandleTask(context, cancellationToken)).ToList();
             var task = await Task.WhenAny(tasks);
-            tasksCancellationToken.ThrowIfCancellationRequested();
-            tasksCancellationSource.Cancel();
-
             index = tasks.IndexOf(task);
         }
 
         cancellationToken.ThrowIfCancellationRequested();
-        return (index, events[index].GetValueAndUpdateWaitHandle(context));
+        return (index, events[index].CleanupAndGetValue(context));
     }
 }
