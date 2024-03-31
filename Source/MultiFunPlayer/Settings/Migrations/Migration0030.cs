7a3daf30a3f86ae29d212821921d0fa24f25a7c5
Yoooi <kamos21@protonmail.com>
2024-03-31 16:38:53 +0200

    Add raw to L0 funscript names in custom devices

---
+++ Source/MultiFunPlayer/Settings/Migrations/Migration0030.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using NLog;
+
+namespace MultiFunPlayer.Settings.Migrations;
+
+internal sealed class Migration0030 : AbstractConfigMigration
+{
+    protected override Logger Logger { get; } = LogManager.GetCurrentClassLogger();
+
+    public override void Migrate(JObject settings)
+    {
+        foreach (var funscriptNames in SelectArrays(settings, "$.Devices[?(@.IsDefault == false)].Axes[?(@.Name == 'L0')].FunscriptNames"))
+            if (!funscriptNames.ToObject<List<string>>().Contains("raw"))
+                AddTokenToContainer("raw", funscriptNames);
+
+        base.Migrate(settings);
+    }
+}
