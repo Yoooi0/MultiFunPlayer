7b51190035edf52d53c76b1e5f5f0f13072ed9a4
Yoooi <kamos21@protonmail.com>
2024-04-23 17:06:15 +0200

    Add more SplittingStringBuffer tests

--- Source/MultiFunPlayer.Tests/SplittingStringBufferTests.cs
+++ Source/MultiFunPlayer.Tests/SplittingStringBufferTests.cs
@@ -4,35 +4,56 @@ namespace MultiFunPlayer.Tests;
 
 public class SplittingStringBufferTests
 {
+    public static IEnumerable<object[]> SeparatorInputAndExpectedOutputs => [
+        ['|', "a|b|", new string[]{ "a", "b" }],
+        ['|', "a|b||", new string[]{ "a", "b", "" }],
+        ['|', "a||b", new string[]{ "a", "" }],
+        ['|', "|a|b", new string[]{ "", "a" }],
+        ['|', "||a|b", new string[]{ "", "", "a" }],
+        ['|', "|a|b|", new string[]{ "", "a", "b" }],
+        ['|', "||a||b||", new string[]{ "", "", "a", "", "b", "" }],
+        ['|', "a|bbbbbbbbbbbbbbbbbbbb", new string[] { "a" }],
+        ['|', "aaaaaaaaaaaaaaaaaaaa|b", new string[] { "aaaaaaaaaaaaaaaaaaaa" }],
+        ['|', "a|bbbbbbbbbbbbbbbbbbbb|", new string[] { "a", "bbbbbbbbbbbbbbbbbbbb" }],
+        ['|', "aaaaaaaaaaaaaaaaaaaa|b|", new string[] { "aaaaaaaaaaaaaaaaaaaa", "b" }],
+    ];
+
+    [Theory]
+    [MemberData(nameof(SeparatorInputAndExpectedOutputs))]
+    public void InputHasExpectedOutputsWhenConsumed(char separator, string input, string[] expectedOutputs)
+    {
+        var buffer = new SplittingStringBuffer(separator);
+        buffer.Push(input);
+
+        var outputs = buffer.Consume().ToArray();
+        Assert.Equal(expectedOutputs, outputs);
+    }
+
     public static IEnumerable<object[]> SeparatorInputsAndExpectedOutputs => [
-        ['|', new string[] { "a|b|" }, new string[]{ "a", "b" }],
-        ['|', new string[] { "a|b||" }, new string[]{ "a", "b", "" }],
-        ['|', new string[] { "a||b" }, new string[]{ "a", "" }],
-        ['|', new string[] { "|a|b" }, new string[]{ "", "a" }],
-        ['|', new string[] { "||a|b" }, new string[]{ "", "", "a" }],
-        ['|', new string[] { "|a|b|" }, new string[]{ "", "a", "b" }],
-        ['|', new string[] { "||a||b||" }, new string[]{ "", "", "a", "", "b", "" }],
-        ['|', new string[] { "a|bbbbbbbbbbbbbbbbbbbb" }, new string[] { "a" }],
-        ['|', new string[] { "aaaaaaaaaaaaaaaaaaaa|b" }, new string[] { "aaaaaaaaaaaaaaaaaaaa" }],
-        ['|', new string[] { "a|bbbbbbbbbbbbbbbbbbbb|" }, new string[] { "a", "bbbbbbbbbbbbbbbbbbbb" }],
-        ['|', new string[] { "aaaaaaaaaaaaaaaaaaaa|b|" }, new string[] { "aaaaaaaaaaaaaaaaaaaa", "b" }],
-        ['|', new string[] { "a|", "b|" }, new string[] { "a", "b" }],
-        ['|', new string[] { "a||", "b|" }, new string[] { "a", "", "b" }],
-        ['|', new string[] { "a", "|", "bbbbbbbbbbbbbbbbbbbb", "|" }, new string[] { "a", "bbbbbbbbbbbbbbbbbbbb" }],
-        ['|', new string[] { "aaaaaaaaaaaaaaaaaaaa", "|", "b", "|" }, new string[] { "aaaaaaaaaaaaaaaaaaaa", "b" }],
-        ['|', new string[] { "a", "|", "bbbbbbbbbbbbbbbbbbbb" }, new string[] { "a" }],
-        ['|', new string[] { "aaaaaaaaaaaaaaaaaaaa", "|", "b" }, new string[] { "aaaaaaaaaaaaaaaaaaaa" }],
+        ['|', new string[] { "a|", "b|" }, new string[][] { ["a"] , ["b"] }],
+        ['|', new string[] { "a||", "b|" }, new string[][] { ["a", ""], ["b"] }],
+        ['|', new string[] { "a", "|", "bbbbbbbbbbbbbbbbbbbb", "|" }, new string[][] { [], ["a"], [], ["bbbbbbbbbbbbbbbbbbbb"] }],
+        ['|', new string[] { "aaaaaaaaaaaaaaaaaaaa", "|", "b", "|" }, new string[][] { [], ["aaaaaaaaaaaaaaaaaaaa"], [], ["b"] }],
+        ['|', new string[] { "a", "|", "bbbbbbbbbbbbbbbbbbbb" }, new string[][] { [], ["a"], [] }],
+        ['|', new string[] { "aaaaaaaaaaaaaaaaaaaa", "|", "b" }, new string[][] { [], ["aaaaaaaaaaaaaaaaaaaa"], [] }],
     ];
 
     [Theory]
     [MemberData(nameof(SeparatorInputsAndExpectedOutputs))]
-    public void ConsumedInputsHaveExpectedOutputsWhenConsumed(char separator, string[] inputs, string[] expectedOutputs)
+    public void InputsHaveExpectedOutputWhenEachConsumed(char separator, string[] inputs, string[][] expectedOutputsPerInput)
     {
         var buffer = new SplittingStringBuffer(separator);
-        foreach(var input in inputs)
+
+        var expectedOuputsEnumerator = expectedOutputsPerInput.GetEnumerator();
+        foreach (var input in inputs)
+        {
             buffer.Push(input);
 
-        var outputs = buffer.Consume().ToArray();
-        Assert.Equal(expectedOutputs, outputs);
+            expectedOuputsEnumerator.MoveNext();
+            var expectedOutputs = expectedOuputsEnumerator.Current;
+
+            var outputs = buffer.Consume().ToArray();
+            Assert.Equal(expectedOutputs, outputs);
+        }
     }
 }
