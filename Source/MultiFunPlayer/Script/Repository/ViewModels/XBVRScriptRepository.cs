5ba038d41b8d56aca80c135d31c99084f1717fdf
Yoooi <kamos21@protonmail.com>
2024-05-27 21:52:20 +0200

    Simplify XBVR repository matching by using currently playing file id

--- Source/MultiFunPlayer/Script/Repository/ViewModels/XBVRScriptRepository.cs
+++ Source/MultiFunPlayer/Script/Repository/ViewModels/XBVRScriptRepository.cs
@@ -1,4 +1,4 @@
-using MultiFunPlayer.Common;
+﻿using MultiFunPlayer.Common;
 using MultiFunPlayer.MediaSource.MediaResource;
 using Newtonsoft.Json;
 using NLog;
@@ -15,8 +15,8 @@ internal sealed class XBVRScriptRepository : AbstractScriptRepository
     private static Logger Logger { get; } = LogManager.GetCurrentClassLogger();
 
     [JsonProperty] public Uri ServerBaseUri { get; set; } = new Uri("http://127.0.0.1:9999");
-    [JsonProperty] public XBVRVideoMatchType VideoMatchType { get; set; } = XBVRVideoMatchType.UseFirstMatchOnly;
-    [JsonProperty] public XBVRScriptMatchType ScriptMatchType { get; set; } = XBVRScriptMatchType.MatchAllUseFirst;
+    [JsonProperty] public XBVRLocalMatchType LocalMatchType { get; set; } = XBVRLocalMatchType.MatchToCurrentFile;
+    [JsonProperty] public XBVRDmsMatchType DmsMatchType { get; set; } = XBVRDmsMatchType.MatchToCurrentFile;
 
     public override async ValueTask<Dictionary<DeviceAxis, IScriptResource>> SearchForScriptsAsync(
         MediaResourceInfo mediaResource, IEnumerable<DeviceAxis> axes, ILocalScriptRepository localRepository, CancellationToken token)
@@ -24,11 +24,12 @@ internal sealed class XBVRScriptRepository : AbstractScriptRepository
         if (ServerBaseUri == null)
             return [];
 
-        var sceneId = await TryGetSceneId(mediaResource);
-        if (string.IsNullOrWhiteSpace(sceneId))
-            return [];
+        Logger.Trace("Searching for ids in \"{0}\"", mediaResource.Path);
+        var (sceneId, fileId) = await TryGetIds(mediaResource);
 
-        Logger.Debug("Found XBVR scene id [Id: {0}]", sceneId);
+        Logger.Debug("Found ids [SceneId: \"{0}\", FileId: \"{1}\"]", sceneId, fileId);
+        if (sceneId == null || fileId == null)
+            return [];
 
         using var client = NetUtils.CreateHttpClient();
         var result = new Dictionary<DeviceAxis, IScriptResource>();
@@ -36,45 +37,63 @@ internal sealed class XBVRScriptRepository : AbstractScriptRepository
         var response = await client.GetStringAsync(uri, token);
 
         Logger.Trace("Received XBVR scene content \"{0}\"", response);
-
         var metadata = JsonConvert.DeserializeObject<SceneMetadata>(response);
-        _ = TryMatchLocal(metadata, axes, result, localRepository) || await TryMatchDms(metadata, axes, result, client, token);
+        if (metadata?.Files == null || metadata.Files.Count == 0)
+            return result;
+
+        var currentFile = metadata.Files.Find(f => f.Id == fileId);
+        if (currentFile == null)
+            return result;
+
+        Logger.Debug("Trying to match scripts for video file [Path: {0}, Filename: {1}]", currentFile.Path, currentFile.Filename);
+        if (!TryMatchLocal(currentFile, axes, result, localRepository))
+            await TryMatchDms(metadata, currentFile, axes, result, client, token);
+
         return result;
     }
 
-    private async ValueTask<string> TryGetSceneId(MediaResourceInfo mediaResource)
+    private async ValueTask<(int? SceneId, int? FileId)> TryGetIds(MediaResourceInfo mediaResource)
     {
-        if (mediaResource.IsFile)
+        if (!mediaResource.IsUrl)
+            return (null, null);
+
+        var mediaResourceUri = new Uri(mediaResource.Path);
+        if (!await PointsToTheSameEndpoint(mediaResourceUri, ServerBaseUri))
         {
-            var match = Regex.Match(mediaResource.Name, @"^(?<id>\d+) - .+");
-            if (!match.Success)
-                return null;
-
-            return match.Groups["id"].Value;
-        }
-        else if (mediaResource.IsUrl)
-        {
-            var mediaResourceUri = new Uri(mediaResource.Path);
-            if (!await PointsToTheSameEndpoint(mediaResourceUri, ServerBaseUri))
-            {
-                Logger.Debug("Ignoring \"{0}\" resource because it does not point to \"{1}\"", mediaResource.Path, ServerBaseUri);
-                return null;
-            }
-
-            var pathAndQuery = mediaResourceUri.GetComponents(UriComponents.PathAndQuery, UriFormat.Unescaped);
-
-            // <endpoint>/res?scene=<sceneId>
-            var match = Regex.Match(pathAndQuery, "scene=(?<id>.+?)(?>$|&)");
-            if (match.Success)
-                return match.Groups["id"].Value;
-
-            // <endpoint>/api/dms/file/<fileId>/<sceneId> - <title>
-            match = Regex.Match(pathAndQuery, @"api\/dms\/file\/\d+\/(?<id>\d+) - .+");
-            if (match.Success)
-                return match.Groups["id"].Value;
+            Logger.Debug("Ignoring \"{0}\" resource because it does not point to \"{1}\"", mediaResource.Path, ServerBaseUri);
+            return (null, null);
         }
 
-        return null;
+        var pathAndQuery = mediaResourceUri.GetComponents(UriComponents.PathAndQuery, UriFormat.Unescaped);
+
+        // DLNA: <endpoint>/res?scene=<sceneId>&file=<fileId>
+        var dlnaSceneMatch = Regex.Match(pathAndQuery, @"res\?.*scene=(?<id>\d+)(?>$|&)");
+        if (dlnaSceneMatch.Success)
+        {
+            var sceneId = int.Parse(dlnaSceneMatch.Groups["id"].Value);
+            var dlnaFileMatch = Regex.Match(pathAndQuery, @"res\?.*file=(?<id>\d+)(?>$|&)");
+            var fileId = dlnaFileMatch.Success ? int.Parse(dlnaFileMatch.Groups["id"].Value) : 0;
+            return (sceneId, fileId);
+        }
+
+        // DeoVR: <endpoint>/api/dms/file/<fileId>/<sceneId> - <title>
+        var deovrMatch = Regex.Match(pathAndQuery, @"api\/dms\/file\/(?<fileId>\d+)\/(?<sceneId>\d+) - .+");
+        if (deovrMatch.Success)
+        {
+            var sceneId = int.Parse(deovrMatch.Groups["sceneId"].Value);
+            var fileId = int.Parse(deovrMatch.Groups["fileId"].Value);
+            return (sceneId, fileId);
+        }
+
+        // HereSphere: <endpoint>/api/dms/file/<fileId>
+        var heresphereMatch = Regex.Match(pathAndQuery, @"api\/dms\/file\/(?<id>\d+)(?>\/?$|\?)");
+        if (heresphereMatch.Success)
+        {
+            var fileId = int.Parse(heresphereMatch.Groups["id"].Value);
+            return (null, fileId);
+        }
+
+        return (null, null);
 
         static async Task<bool> PointsToTheSameEndpoint(Uri resourceUri, Uri serverUri)
         {
@@ -92,76 +111,48 @@ internal sealed class XBVRScriptRepository : AbstractScriptRepository
         }
     }
 
-    private bool TryMatchLocal(SceneMetadata metadata, IEnumerable<DeviceAxis> axes, Dictionary<DeviceAxis, IScriptResource> result, ILocalScriptRepository localRepository)
+    private bool TryMatchLocal(SceneFile currentFile, IEnumerable<DeviceAxis> axes, Dictionary<DeviceAxis, IScriptResource> result, ILocalScriptRepository localRepository)
     {
-        if (metadata?.Files == null)
+        if (LocalMatchType == XBVRLocalMatchType.None)
             return false;
 
-        foreach (var videoFile in metadata.Files.Where(f => f.Type == "video"))
+        if (LocalMatchType == XBVRLocalMatchType.MatchToCurrentFile)
         {
-            Logger.Debug("Trying to match scripts for video file [Path: {0}, Filename: {1}]", videoFile.Path, videoFile.Filename);
-            var searchResult = localRepository.SearchForScripts(videoFile.Filename, videoFile.Path, axes);
-            if (searchResult.Count == 0)
-                continue;
-
-            if (VideoMatchType == XBVRVideoMatchType.UseFirstMatchOnly)
-            {
-                result.Merge(searchResult);
-                return true;
-            }
-
-            foreach (var (axis, resource) in searchResult)
-            {
-                if (VideoMatchType == XBVRVideoMatchType.MatchAllOverwrite && result.TryGetValue(axis, out var existingResource))
-                    Logger.Debug("Overwriting already matched script [From: {0}, To: {1}]", existingResource.Name, resource.Name);
-
-                var validMatch = (VideoMatchType == XBVRVideoMatchType.MatchAllUseFirst && !result.ContainsKey(axis))
-                              || (VideoMatchType == XBVRVideoMatchType.MatchAllOverwrite);
-
-                if (validMatch)
-                {
-                    Logger.Debug("Matched \"{0}\" to {1} axis", resource.Name, axis);
-                    result[axis] = resource;
-                }
-            }
+            var searchResult = localRepository.SearchForScripts(currentFile.Filename, currentFile.Path, axes);
+            result.Merge(searchResult);
         }
 
         return result.Count > 0;
     }
 
-    private async Task<bool> TryMatchDms(SceneMetadata metadata, IEnumerable<DeviceAxis> axes, Dictionary<DeviceAxis, IScriptResource> result, HttpClient client, CancellationToken token)
+    private async Task<bool> TryMatchDms(SceneMetadata metadata, SceneFile currentFile, IEnumerable<DeviceAxis> axes, Dictionary<DeviceAxis, IScriptResource> result, HttpClient client, CancellationToken token)
     {
-        if (metadata?.Files == null)
+        if (DmsMatchType == XBVRDmsMatchType.None)
             return false;
 
-        var matchedFiles = new Dictionary<DeviceAxis, SceneFile>();
-        foreach (var scriptFile in metadata.Files.Where(f => f.Type == "script"))
+        var resultFiles = new Dictionary<DeviceAxis, SceneFile>();
+        var scriptFiles = metadata.Files.Where(f => f.Type == "script");
+
+        if (DmsMatchType == XBVRDmsMatchType.MatchToCurrentFile)
         {
-            Logger.Debug("Trying to match axes for script file [Path: {0}, Filename: {1}, IsSelected: {2}]", scriptFile.Path, scriptFile.Filename, scriptFile.IsSelected);
-            foreach (var axis in DeviceAxisUtils.FindAxesMatchingName(axes, scriptFile.Filename))
-            {
-                if (ScriptMatchType == XBVRScriptMatchType.MatchAllOverwrite && matchedFiles.TryGetValue(axis, out var existingScriptFile))
-                    Logger.Debug("Overwriting already matched {0} script file [From: {1}, To: {2}]", axis, existingScriptFile.Filename, scriptFile.Filename);
-
-                var validMatch = (ScriptMatchType == XBVRScriptMatchType.MatchSelectedOnly && scriptFile.IsSelected)
-                              || (ScriptMatchType == XBVRScriptMatchType.MatchAllUseFirst && !matchedFiles.ContainsKey(axis))
-                              || (ScriptMatchType == XBVRScriptMatchType.MatchAllOverwrite);
-
-                if (validMatch)
-                {
-                    Logger.Debug("Matched \"{0}\" to {1} axis", scriptFile.Filename, axis);
-                    matchedFiles[axis] = scriptFile;
-                }
-            }
+            foreach (var axis in axes)
+                foreach (var matchedScript in DeviceAxisUtils.FindNamesMatchingAxis(axis, scriptFiles, s => s.Filename, currentFile.Filename))
+                    AddToResult(resultFiles, axis, matchedScript);
+        }
+        else if (DmsMatchType == XBVRDmsMatchType.MatchSelectedOnly)
+        {
+            foreach (var scriptFile in scriptFiles.Where(f => f.IsSelected))
+                foreach (var axis in DeviceAxisUtils.FindAxesMatchingName(axes, scriptFile.Filename))
+                    AddToResult(resultFiles, axis, scriptFile);
         }
 
-        foreach (var (axis, script) in matchedFiles)
+        foreach (var (axis, scriptFile) in resultFiles)
         {
-            var scriptUri = new Uri(ServerBaseUri, $"/api/dms/file/{script.Id}");
+            var scriptUri = new Uri(ServerBaseUri, $"/api/dms/file/{scriptFile.Id}");
             Logger.Trace("Downloading {0} script file [Uri: {1}]", axis, scriptUri);
 
             var scriptStream = await client.GetStreamAsync(scriptUri, token);
-            var readerResult = FunscriptReader.Default.FromStream(script.Filename, scriptUri.ToString(), scriptStream);
+            var readerResult = FunscriptReader.Default.FromStream(scriptFile.Filename, scriptUri.ToString(), scriptStream);
             if (!readerResult.IsSuccess)
                 continue;
 
@@ -172,28 +163,34 @@ internal sealed class XBVRScriptRepository : AbstractScriptRepository
         }
 
         return result.Count > 0;
+
+        static void AddToResult(IDictionary<DeviceAxis, SceneFile> result, DeviceAxis axis, SceneFile resource)
+        {
+            if (result.TryAdd(axis, resource))
+                Logger.Debug("Matched {0} script to [Path: \"{1}\", Filename: \"{2}\"]", axis, resource?.Path, resource?.Filename);
+            else
+                Logger.Debug("Ignoring {0} script [Path: \"{1}\", Filename: \"{2}\"] because {0} is already matched to a script", axis, resource?.Path, resource?.Filename);
+        }
     }
 
     private sealed record SceneMetadata([JsonProperty("file")] List<SceneFile> Files);
     private sealed record SceneFile(int Id, string Path, string Filename, string Type, [JsonProperty("is_selected_script")] bool IsSelected);
 }
 
-internal enum XBVRScriptMatchType
+internal enum XBVRLocalMatchType
 {
-    [Description("Try to match all scripts, for each axis only use the first matched script")]
-    MatchAllUseFirst,
-    [Description("Try to match all scripts, overwrite if multiple scripts match an axis")]
-    MatchAllOverwrite,
-    [Description("Try to only match scripts selected in XBVR, overwrite if multiple scripts match an axis")]
-    MatchSelectedOnly
+    [Description("Don't match scripts using local repository")]
+    None,
+    [Description("Match scripts based on currently playing XBVR file using local repository")]
+    MatchToCurrentFile
 }
 
-internal enum XBVRVideoMatchType
+internal enum XBVRDmsMatchType
 {
-    [Description("Use first video that matches at least one axis")]
-    UseFirstMatchOnly,
-    [Description("Try to match all videos, for each axis use first matched script")]
-    MatchAllUseFirst,
-    [Description("Try to match all videos, overwrite if multiple scripts match an axis")]
-    MatchAllOverwrite
+    [Description("Don't match scripts using XBVR dms")]
+    None,
+    [Description("Match scripts based on currently playing XBVR file using XBVR dms")]
+    MatchToCurrentFile,
+    [Description("Match only scripts selected in XBVR using XBVR dms")]
+    MatchSelectedOnly
 }
