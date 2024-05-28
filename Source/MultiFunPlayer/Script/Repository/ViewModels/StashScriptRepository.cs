f073dae71ec12b1330d99e97a767cb4bd3ab8008
Yoooi <kamos21@protonmail.com>
2024-05-28 22:10:15 +0200

    Simplify Stash repository matching by only using the primary scene file

--- Source/MultiFunPlayer/Script/Repository/ViewModels/StashScriptRepository.cs
+++ Source/MultiFunPlayer/Script/Repository/ViewModels/StashScriptRepository.cs
@@ -18,8 +18,9 @@ internal sealed class StashScriptRepository : AbstractScriptRepository
 
     [JsonProperty] public Uri ServerBaseUri { get; set; } = new Uri("http://127.0.0.1:9999");
     [JsonProperty] public string ApiKey { get; set; } = null;
-    [JsonProperty] public StashVideoMatchType VideoMatchType { get; set; } = StashVideoMatchType.UseFirstMatchOnly;
-    [JsonProperty] public DeviceAxis ScriptMatchAxis { get; set; } = DeviceAxis.All.FirstOrDefault();
+    [JsonProperty] public StashLocalMatchType LocalMatchType { get; set; } = StashLocalMatchType.MatchToCurrentFile;
+    [JsonProperty] public StashDmsMatchType DmsMatchType { get; set; } = StashDmsMatchType.MatchToAxis;
+    [JsonProperty] public DeviceAxis DmsMatchAxis { get; set; } = DeviceAxis.All.FirstOrDefault();
 
     public override async ValueTask<Dictionary<DeviceAxis, IScriptResource>> SearchForScriptsAsync(
         MediaResourceInfo mediaResource, IEnumerable<DeviceAxis> axes, ILocalScriptRepository localRepository, CancellationToken token)
@@ -27,11 +28,12 @@ internal sealed class StashScriptRepository : AbstractScriptRepository
         if (ServerBaseUri == null)
             return [];
 
+        Logger.Trace("Searching for scene id in \"{0}\"", mediaResource.Path);
         var sceneId = await TryGetSceneId(mediaResource);
+
+        Logger.Debug("Found scene id [Id: {0}]", sceneId);
         if (string.IsNullOrWhiteSpace(sceneId))
             return [];
-
-        Logger.Debug("Found Stash scene id [Id: {0}]", sceneId);
 
         using var client = NetUtils.CreateHttpClient();
         if (!string.IsNullOrWhiteSpace(ApiKey))
@@ -47,47 +49,43 @@ internal sealed class StashScriptRepository : AbstractScriptRepository
             Content = new StringContent(query, Encoding.UTF8, "application/json")
         };
 
+        Logger.Trace("Sending query \"{0}\"", query);
         var response = await client.SendAsync(request, token);
         var content = await response.Content.ReadAsStringAsync(token);
 
-        Logger.Trace("Received Stash api response \"{0}\"", content);
+        Logger.Trace("Received query response \"{0}\"", content);
 
         var queryRespone = JsonConvert.DeserializeObject<QueryResponse>(content);
-        _ = TryMatchLocal(queryRespone, axes, result, localRepository) || await TryMatchDms(queryRespone, result, client, token);
+        var primaryFile = queryRespone?.Data?.FindScene?.Files?.FirstOrDefault();
+        if (!TryMatchLocal(primaryFile, axes, result, localRepository))
+            await TryMatchDms(queryRespone, result, client, token);
+
         return result;
     }
 
     private async ValueTask<string> TryGetSceneId(MediaResourceInfo mediaResource)
     {
-        if (mediaResource.IsFile)
-        {
-            var match = Regex.Match(mediaResource.Name, @"^(?<id>\d+) - .+");
-            if (!match.Success)
-                return null;
+        if (!mediaResource.IsUrl)
+            return null;
 
+        var mediaResourceUri = new Uri(mediaResource.Path);
+        if (!await PointsToTheSameEndpoint(mediaResourceUri, ServerBaseUri))
+        {
+            Logger.Debug("Ignoring \"{0}\" resource because it does not point to \"{1}\"", mediaResource.Path, ServerBaseUri);
+            return null;
+        }
+
+        var pathAndQuery = mediaResourceUri.GetComponents(UriComponents.PathAndQuery, UriFormat.Unescaped);
+
+        // DLNA: <endpoint>/res?scene=<sceneId>
+        var match = Regex.Match(pathAndQuery, @"res\?.*scene=(?<id>\d+?)(?>$|&)");
+        if (match.Success)
             return match.Groups["id"].Value;
-        }
-        else if (mediaResource.IsUrl)
-        {
-            var mediaResourceUri = new Uri(mediaResource.Path);
-            if (!await PointsToTheSameEndpoint(mediaResourceUri, ServerBaseUri))
-            {
-                Logger.Debug("Ignoring \"{0}\" resource because it does not point to \"{1}\"", mediaResource.Path, ServerBaseUri);
-                return null;
-            }
 
-            var pathAndQuery = mediaResourceUri.GetComponents(UriComponents.PathAndQuery, UriFormat.Unescaped);
-
-            // <endpoint>/res?scene=<sceneId>
-            var match = Regex.Match(pathAndQuery, @"scene=(?<id>\d+?)(?>$|&)");
-            if (match.Success)
-                return match.Groups["id"].Value;
-
-            // <endpoint>/scene/<sceneId>/stream
-            match = Regex.Match(pathAndQuery, @"scene\/(?<id>\d+)\/stream");
-            if (match.Success)
-                return match.Groups["id"].Value;
-        }
+        // Stream: <endpoint>/scene/<sceneId>/stream
+        match = Regex.Match(pathAndQuery, @"scene\/(?<id>\d+)\/stream");
+        if (match.Success)
+            return match.Groups["id"].Value;
 
         return null;
 
@@ -107,41 +105,22 @@ internal sealed class StashScriptRepository : AbstractScriptRepository
         }
     }
 
-    private bool TryMatchLocal(QueryResponse queryRespone, IEnumerable<DeviceAxis> axes, Dictionary<DeviceAxis, IScriptResource> result, ILocalScriptRepository localRepository)
+    private bool TryMatchLocal(QueryFile primaryFile, IEnumerable<DeviceAxis> axes, Dictionary<DeviceAxis, IScriptResource> result, ILocalScriptRepository localRepository)
     {
-        if (queryRespone?.Data?.FindScene?.Files == null)
+        if (primaryFile == null)
+            return false;
+        if (LocalMatchType == StashLocalMatchType.None)
             return false;
 
-        foreach (var file in queryRespone.Data.FindScene.Files)
+        if (LocalMatchType == StashLocalMatchType.MatchToCurrentFile)
         {
-            var directory = Path.GetDirectoryName(file.Path);
-            var fileName = Path.GetFileName(file.Path);
+            var source = Path.GetDirectoryName(primaryFile.Path);
+            var name = Path.GetFileName(primaryFile.Path);
 
-            Logger.Debug("Trying to match scripts for video file [Directory: {0}, Filename: {1}]", directory, fileName);
-            var searchResult = localRepository.SearchForScripts(fileName, directory, axes);
-            if (searchResult.Count == 0)
-                continue;
+            Logger.Trace("Trying to match scripts to primary file using local repository [Source: {0}, Name: {1}]", source, name);
 
-            if (VideoMatchType == StashVideoMatchType.UseFirstMatchOnly)
-            {
-                result.Merge(searchResult);
-                return true;
-            }
-
-            foreach (var (axis, resource) in searchResult)
-            {
-                if (VideoMatchType == StashVideoMatchType.MatchAllOverwrite && result.TryGetValue(axis, out var existingResource))
-                    Logger.Debug("Overwriting already matched script [From: {0}, To: {1}]", existingResource.Name, resource.Name);
-
-                var validMatch = (VideoMatchType == StashVideoMatchType.MatchAllUseFirst && !result.ContainsKey(axis))
-                              || (VideoMatchType == StashVideoMatchType.MatchAllOverwrite);
-
-                if (validMatch)
-                {
-                    Logger.Debug("Matched \"{0}\" to {1} axis", resource.Name, axis);
-                    result[axis] = resource;
-                }
-            }
+            var searchResult = localRepository.SearchForScripts(name, source, axes);
+            result.Merge(searchResult);
         }
 
         return result.Count > 0;
@@ -149,6 +128,8 @@ internal sealed class StashScriptRepository : AbstractScriptRepository
 
     private async Task<bool> TryMatchDms(QueryResponse queryRespone, Dictionary<DeviceAxis, IScriptResource> result, HttpClient client, CancellationToken token)
     {
+        if (DmsMatchType == StashDmsMatchType.None)
+            return false;
         if (queryRespone?.Data?.FindScene?.Paths?.Funscript == null)
             return false;
 
@@ -159,7 +140,7 @@ internal sealed class StashScriptRepository : AbstractScriptRepository
             scriptName += $" - {queryRespone.Data.FindScene.Title}";
         scriptName += ".funscript";
 
-        Logger.Trace("Downloading script file [Uri: {0}]", scriptUri);
+        Logger.Trace("Downloading scene script file [Uri: {0}]", scriptUri);
 
         var scriptStream = await client.GetStreamAsync(scriptUri, token);
         var readerResult = FunscriptReader.Default.FromStream(scriptName, ServerBaseUri.ToString(), scriptStream);
@@ -167,9 +148,15 @@ internal sealed class StashScriptRepository : AbstractScriptRepository
             return false;
 
         if (readerResult.IsMultiAxis)
+        {
+            Logger.Trace("Matching scene multi-axis script");
             result.Merge(readerResult.Resources);
-        else if (ScriptMatchAxis != null)
-            result[ScriptMatchAxis] = readerResult.Resource;
+        }
+        else if (DmsMatchAxis != null)
+        {
+            Logger.Trace("Matching scene script to {0}", DmsMatchAxis);
+            result[DmsMatchAxis] = readerResult.Resource;
+        }
 
         return result.Count > 0;
     }
@@ -181,12 +168,18 @@ internal sealed class StashScriptRepository : AbstractScriptRepository
     private sealed record QueryPaths(string Funscript);
 }
 
-internal enum StashVideoMatchType
+internal enum StashLocalMatchType
 {
-    [Description("Use first video that matches at least one axis")]
-    UseFirstMatchOnly,
-    [Description("Try to match all videos, for each axis use first matched script")]
-    MatchAllUseFirst,
-    [Description("Try to match all videos, overwrite if multiple scripts match an axis")]
-    MatchAllOverwrite
+    [Description("Don't match scripts using local repository")]
+    None,
+    [Description("Match scripts based on currently playing Stash file using local repository")]
+    MatchToCurrentFile
+}
+
+internal enum StashDmsMatchType
+{
+    [Description("Don't match scripts using Stash dms")]
+    None,
+    [Description("Match scene script to selected axis using Stash dms")]
+    MatchToAxis
 }
