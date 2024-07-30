2167881ee35c2c3308420e7b73b27b8f1365fa59
Yoooi <kamos21@protonmail.com>
2024-07-31 00:12:52 +0200

    Allow invoking ButtonLongPress shortcut while holding

--- Source/MultiFunPlayer/Shortcut/ButtonLongPressShortcut.cs
+++ Source/MultiFunPlayer/Shortcut/ButtonLongPressShortcut.cs
@@ -1,4 +1,4 @@
-﻿using MultiFunPlayer.Input;
+using MultiFunPlayer.Input;
 using System.ComponentModel;
 
 namespace MultiFunPlayer.Shortcut;
@@ -9,6 +9,7 @@ internal sealed class ButtonLongPressShortcut(IShortcutActionRunner actionRunner
 {
     public int MinimumHoldDuration { get; set; } = 1000;
     public int MaximumHoldDuration { get; set; } = -1;
+    public ButtonLongPressInvokeType InvokeType { get; set; } = ButtonLongPressInvokeType.OnRelease;
 
     private int _pressTime;
 
@@ -17,18 +18,34 @@ internal sealed class ButtonLongPressShortcut(IShortcutActionRunner actionRunner
         if (gesture.State && _pressTime == 0)
         {
             _pressTime = Environment.TickCount;
+
+            if (InvokeType == ButtonLongPressInvokeType.WhileHolding)
+                Delay(MinimumHoldDuration, () => Invoke(SimpleInputGestureData.Default));
         }
         else if (!gesture.State && _pressTime > 0)
         {
             var duration = Environment.TickCount - _pressTime;
-
             _pressTime = 0;
-            if (duration < MinimumHoldDuration)
-                return;
-            if (MaximumHoldDuration > MinimumHoldDuration && duration > MaximumHoldDuration)
+
+            if (InvokeType == ButtonLongPressInvokeType.WhileHolding)
+            {
+                CancelDelay();
+            }
+            else if (InvokeType == ButtonLongPressInvokeType.OnRelease)
+            {
+                if (duration < MinimumHoldDuration)
+                    return;
+                if (MaximumHoldDuration > MinimumHoldDuration && duration > MaximumHoldDuration)
                 return;
 
-            Invoke(SimpleInputGestureData.Default);
+                Invoke(SimpleInputGestureData.Default);
+            }
         }
     }
+}
+
+internal enum ButtonLongPressInvokeType
+{
+    OnRelease,
+    WhileHolding
 }
